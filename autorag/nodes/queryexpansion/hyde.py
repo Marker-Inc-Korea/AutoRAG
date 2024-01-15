5241bf9eb7502d0a47bf7649843764d73742f91b
kimbwook <bwook00@naver.com>
2024-01-15 16:02:57 +0900

    delete default llm(GPT 3.5 turbo)

--- autorag/nodes/queryexpansion/hyde.py
+++ autorag/nodes/queryexpansion/hyde.py
@@ -2,19 +2,18 @@ import asyncio
 from typing import List
 
 from llama_index.llms.llm import BaseLLM
-from llama_index.llms.openai import OpenAI
 
 hyde_prompt = "Please write a passage to answer the question"
 
 
-def hyde(queries: List[str], llm: BaseLLM = OpenAI(max_tokens=64),
+def hyde(queries: List[str], llm: BaseLLM,
          prompt: str = hyde_prompt) -> List[List[str]]:
     """
     HyDE, which inspired by "Precise Zero-shot Dense Retrieval without Relevance Labels" (https://arxiv.org/pdf/2212.10496.pdf)
     LLM model creates hypothetical passage.
     And then, retrieve passages using hypothetical passage as query.
     :param queries: List[str], queries to retrieve.
-    :param llm: llm to use for hypothetical passage generation. HyDE Retrieval supports both chat and completion LLMs.
+    :param llm: llm to use for hypothetical passage generation.
     :param prompt: prompt to use when generating hypothetical passage
     :return: List[List[str]], List of retrieved passages
     """
--- tests/autorag/nodes/queryexpansion/test_hyde.py
+++ tests/autorag/nodes/queryexpansion/test_hyde.py
@@ -1,8 +1,11 @@
+from llama_index.llms.openai import OpenAI
+
 from autorag.nodes.queryexpansion import hyde
 
 sample_query = ["How many members are in Newjeans?"]
 
 
 def test_hyde():
-    result = hyde(sample_query)
+    llm = OpenAI(max_tokens=64)
+    result = hyde(sample_query, llm)
     assert len(result[0]) == 1
