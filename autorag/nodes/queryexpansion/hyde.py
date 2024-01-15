027f7365548ab7f372278fe761946fda4e4bb7b9
kimbwook <bwook00@naver.com>
2024-01-15 15:39:34 +0900

    Add code comments

--- autorag/nodes/queryexpansion/hyde.py
+++ autorag/nodes/queryexpansion/hyde.py
@@ -9,6 +9,15 @@ hyde_prompt = "Please write a passage to answer the question"
 
 def hyde(queries: List[str], llm: BaseLLM = OpenAI(max_tokens=64),
          prompt: str = hyde_prompt) -> List[List[str]]:
+    """
+    HyDE, which inspired by "Precise Zero-shot Dense Retrieval without Relevance Labels" (https://arxiv.org/pdf/2212.10496.pdf)
+    LLM model creates hypothetical passage.
+    And then, retrieve passages using hypothetical passage as query.
+    :param queries: List[str], queries to retrieve.
+    :param llm: llm to use for hypothetical passage generation. HyDE Retrieval supports both chat and completion LLMs.
+    :param prompt: prompt to use when generating hypothetical passage
+    :return: List[List[str]], List of retrieved passages
+    """
     # Run async query_decompose_pure function
     tasks = [hyde_pure(query, llm, prompt) for query in queries]
     loop = asyncio.get_event_loop()
