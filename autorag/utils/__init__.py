bf7f8af095f1ff239d32511a8c4880fb568680c0
jeffrey <vkefhdl1@gmail.com>
2024-01-16 15:44:08 +0900

    validate and cast qa & corpus dataset at utils/preprocess.py

--- autorag/utils/__init__.py
+++ autorag/utils/__init__.py
@@ -1,2 +1,2 @@
-from .validate import validate_qa_dataset, validate_corpus_dataset
+from .preprocess import validate_qa_dataset, validate_corpus_dataset, cast_qa_dataset, cast_corpus_dataset
 from .util import fetch_contents, result_to_dataframe
