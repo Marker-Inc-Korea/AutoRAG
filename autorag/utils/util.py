b55142850e8af7e43dba44a2a98e93da539bce4f
kimbwook <bwook00@naver.com>
2024-01-24 11:58:36 +0000

    Fix IndexError on fetch contents to show description

--- autorag/utils/util.py
+++ autorag/utils/util.py
@@ -3,14 +3,16 @@ import os
 from typing import List, Callable, Dict
 
 import pandas as pd
-import swifter
 
 
 def fetch_contents(corpus_data: pd.DataFrame, ids: List[List[str]]) -> List[List[str]]:
     assert isinstance(ids[0], list), "ids must be a list of list of ids."
     id_df = pd.DataFrame(ids, columns=[f'id_{i}' for i in range(len(ids[0]))])
-    contents_df = id_df.swifter.applymap(
-        lambda x: corpus_data.loc[lambda row: row['doc_id'] == x]['contents'].values[0])
+    try:
+        contents_df = id_df.swifter.applymap(
+            lambda x: corpus_data.loc[lambda row: row['doc_id'] == x]['contents'].values[0])
+    except IndexError:
+        raise IndexError("doc_id does not exist in corpus_data.")
     return contents_df.values.tolist()
 
 
