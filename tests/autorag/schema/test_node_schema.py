e854b36851bc19f1618b9946a4b05bf60f8f338d
jeffrey <vkefhdl1@gmail.com>
2024-01-25 15:46:50 +0000

    fix crucial bug that can't make combinations of multiple modules

--- tests/autorag/schema/test_node_schema.py
+++ tests/autorag/schema/test_node_schema.py
@@ -1,28 +1,46 @@
+from autorag.nodes.retrieval import bm25, vectordb
 from autorag.nodes.retrieval.run import run_retrieval_node
 from autorag.schema import Node
 from autorag.schema.module import Module
 from autorag.schema.node import extract_values_from_nodes, module_type_exists
 
 
-# Test for Node.get_module_node_params method
 def test_get_param_combinations():
     modules = [
         Module.from_dict(
             {'module_type': 'bm25', 'key2': ['value1', 'value2'], 'key3': 'value3', 'key4': ['value4', 'value5']}),
+        Module.from_dict(
+            {'module_type': 'vectordb', 'key5': ['value6', 'value6', 'value7'], 'key6': 'value8'}
+        )
     ]
     node = Node(node_type='retrieval', strategy={'strategy_key': 'strategy_value'}, node_params={'param1': 'value1'},
                 modules=modules)
 
-    module_node_params = node.get_param_combinations()
+    modules, module_node_params = node.get_param_combinations()
 
     assert isinstance(module_node_params, list)
-    assert module_node_params == [{'param1': 'value1', 'key2': 'value1', 'key3': 'value3', 'key4': 'value4'},
-                                  {'param1': 'value1', 'key2': 'value1', 'key3': 'value3', 'key4': 'value5'},
-                                  {'param1': 'value1', 'key2': 'value2', 'key3': 'value3', 'key4': 'value4'},
-                                  {'param1': 'value1', 'key2': 'value2', 'key3': 'value3', 'key4': 'value5'}]
+    assert isinstance(modules, list)
+    assert len(modules) == 6
+    assert modules == [bm25, bm25, bm25, bm25, vectordb, vectordb]
+    bm25_solution = [{'param1': 'value1', 'key2': 'value1', 'key3': 'value3', 'key4': 'value4'},
+                     {'param1': 'value1', 'key2': 'value1', 'key3': 'value3', 'key4': 'value5'},
+                     {'param1': 'value1', 'key2': 'value2', 'key3': 'value3', 'key4': 'value4'},
+                     {'param1': 'value1', 'key2': 'value2', 'key3': 'value3', 'key4': 'value5'}]
+    vectordb_solution = [{'param1': 'value1', 'key5': 'value6', 'key6': 'value8'},
+                         {'param1': 'value1', 'key5': 'value7', 'key6': 'value8'}]
+    for module, module_params in zip(modules, module_node_params):
+        if module == bm25:
+            assert module_params in bm25_solution
+            bm25_solution.pop(bm25_solution.index(module_params))
+        elif module == vectordb:
+            assert module_params in vectordb_solution
+            vectordb_solution.pop(vectordb_solution.index(module_params))
+        else:
+            raise ValueError(f"Module {module} is not supposed to be here.")
+    assert len(bm25_solution) == 0
+    assert len(vectordb_solution) == 0
 
 
-# Test for Node.from_dict class method
 def test_from_dict():
     node_dict = {
         'node_type': 'retrieval',
