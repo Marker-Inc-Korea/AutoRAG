8f6c40854de9cae6ada1c81497fb6e4e21e195d4
kimbwook <bwook00@naver.com>
2024-01-27 17:34:56 +0000

    apply wrapped in test hyde

--- tests/autorag/nodes/queryexpansion/test_hyde.py
+++ tests/autorag/nodes/queryexpansion/test_hyde.py
@@ -7,6 +7,11 @@ sample_query = ["How many members are in Newjeans?", "What is visconde structure
 
 def test_hyde():
     llm = OpenAI(max_tokens=64)
-    result = hyde(sample_query, llm)
+    original_hyde = hyde.__wrapped__
+    result = original_hyde(sample_query, llm, prompt=None)
     assert len(result[0]) == 1
     assert len(result) == 2
+
+
+def test_hyde_node():
+    pass
