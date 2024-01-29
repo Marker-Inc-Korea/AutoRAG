4688e426b5f130b8001f42c813fd8c223cb4b073
jeffrey <vkefhdl1@gmail.com>
2024-01-29 20:04:02 +0000

    add normalize string at util.py

--- autorag/utils/util.py
+++ autorag/utils/util.py
@@ -1,6 +1,8 @@
 import functools
 import itertools
 import os
+import re
+import string
 from typing import List, Callable, Dict, Optional, Any, Collection
 
 import pandas as pd
@@ -131,3 +133,25 @@ def explode(index_values: Collection[Any], explode_values: Collection[Collection
     })
     df = df.explode('explode_values')
     return df['index_values'].tolist(), df['explode_values'].tolist()
+
+
+def normalize_string(s: str) -> str:
+    """
+    Taken from the official evaluation script for v1.1 of the SQuAD dataset.
+    Lower text and remove punctuation, articles and extra whitespace.
+    """
+
+    def remove_articles(text):
+        return re.sub(r"\b(a|an|the)\b", " ", text)
+
+    def white_space_fix(text):
+        return " ".join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
