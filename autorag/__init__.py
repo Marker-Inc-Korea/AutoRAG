cc4d9973052b39852923631271ee7bc7066c203e
kimbwook <bwook00@naver.com>
2024-02-03 14:35:50 +0000

    add embedding_models and generator_models

--- autorag/__init__.py
+++ autorag/__init__.py
@@ -5,8 +5,9 @@ import sys
 
 from rich.logging import RichHandler
 
-from llama_index import OpenAIEmbedding
-from llama_index.llms import OpenAI
+from llama_index.embeddings import OpenAIEmbedding, HuggingFaceEmbedding, InstructorEmbedding, OptimumEmbedding
+from llama_index.llms import OpenAI, Anthropic, AzureOpenAI, HuggingFaceLLM, LangChainLLM, GradientBaseModelLLM, \
+    GradientModelAdapterLLM, LiteLLM, LlamaCPP, OpenAILike, OpenLLM, PaLM, PredibaseLLM, Replicate, Xinference
 
 root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 version_path = os.path.join(root_path, 'VERSION')
@@ -16,10 +17,29 @@ with open(version_path, 'r') as f:
 
 embedding_models = {
     'openai': OpenAIEmbedding(),
+    # You can write your own model in this way.
+    'huggingface_baai_bge_small': HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"),
+    'huggingface_cointegrated_rubert_tiny2': HuggingFaceEmbedding(model_name="cointegrated/rubert-tiny2"),
+    'instructor_hkunlp': InstructorEmbedding(model_name="hkunlp/instructor-base"),
+    'optimum_baai_bge_small': OptimumEmbedding.create_and_save_optimum_model("BAAI/bge-small-en-v1.5", "./bge_onnx"),
 }
 
 generator_models = {
     'openai': OpenAI,
+    'anthropic': Anthropic,
+    'azureopenai': AzureOpenAI,
+    'huggingfacellm': HuggingFaceLLM,
+    'langchainllm': LangChainLLM,
+    'gradientbasemodelllm': GradientBaseModelLLM,
+    'gradientmodeladapterllm': GradientModelAdapterLLM,
+    'litellm': LiteLLM,
+    'llamacpp': LlamaCPP,
+    'openailike': OpenAILike,
+    'openllm': OpenLLM,
+    'palm': PaLM,
+    'predibasellm': PredibaseLLM,
+    'replicate': Replicate,
+    'xinference': Xinference,
 }
 
 rich_format = "[%(filename)s:%(lineno)s] >> %(message)s"
