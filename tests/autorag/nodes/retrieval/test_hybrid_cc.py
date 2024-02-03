a62204758ac9c655a54e7c651e4bac7018ac5a28
jeffrey <vkefhdl1@gmail.com>
2024-02-03 10:49:51 +0000

    fix missing __wrapped__ at test_hybrid_cc.py

--- tests/autorag/nodes/retrieval/test_hybrid_cc.py
+++ tests/autorag/nodes/retrieval/test_hybrid_cc.py
@@ -31,7 +31,7 @@ def test_hybrid_cc():
                          [6, 2, 1, 0.5, 0.1],
                          [7, 3, 2, 1.5, 1.1],
                      ])
-    result_id, result_scores = hybrid_cc(sample_ids, sample_scores, top_k=3, weights=(0.3, 0.7))
+    result_id, result_scores = hybrid_cc.__wrapped__(sample_ids, sample_scores, top_k=3, weights=(0.3, 0.7))
     assert result_id[0] == ['id-1', 'id-4', 'id-2']
     assert result_id[1] == ['id-2', 'id-5', 'id-3']
     assert result_scores[0] == pytest.approx([1.0, 0.23792372, 0.175])
