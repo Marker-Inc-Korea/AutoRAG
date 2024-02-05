9eb252604dcb4f9c7a2105422988e6ea3ea31deb
kimbwook <bwook00@naver.com>
2024-02-05 11:04:27 +0000

    add click option update readme code snippet

--- sample_dataset/eli5/load_eli5_dataset.py
+++ sample_dataset/eli5/load_eli5_dataset.py
@@ -1,10 +1,13 @@
 import os
 import pathlib
+import click
 
 from datasets import load_dataset
 
 
-def load_eli5_dataset():
+@click.command()
+@click.option('--save_path', type=str, default=pathlib.PurePath(__file__).parent, help='Path to save sample eli5 dataset.')
+def load_eli5_dataset(save_path):
     # set file path
     file_path = "MarkrAI/eli5_sample_autorag"
 
@@ -12,17 +15,13 @@ def load_eli5_dataset():
     corpus_dataset = load_dataset(file_path, "corpus")['train'].to_pandas()
     qa_dataset = load_dataset(file_path, "qa")['train'].to_pandas()
 
-    # path setting
-    sample_dataset_dir = pathlib.PurePath(__file__).parent.parent
-    project_dir = os.path.join(sample_dataset_dir, "eli5")
-
     # save data
-    if os.path.exists(os.path.join(project_dir, "corpus.parquet")) is True:
+    if os.path.exists(os.path.join(save_path, "corpus.parquet")) is True:
         raise ValueError("corpus.parquet already exists")
-    if os.path.exists(os.path.join(project_dir, "qa.parquet")) is True:
+    if os.path.exists(os.path.join(save_path, "qa.parquet")) is True:
         raise ValueError("qa.parquet already exists")
-    corpus_dataset.to_parquet(os.path.join(project_dir, "corpus.parquet"), index=False)
-    qa_dataset.to_parquet(os.path.join(project_dir, "qa.parquet"), index=False)
+    corpus_dataset.to_parquet(os.path.join(save_path, "corpus.parquet"))
+    qa_dataset.to_parquet(os.path.join(save_path, "qa.parquet"))
 
 
 if __name__ == '__main__':
