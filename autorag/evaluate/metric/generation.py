40c616d4d2c8beffe2c13cef0c0604a9eab3fd33
jeffrey <vkefhdl1@gmail.com>
2024-02-09 14:00:07 +0000

    add sem score metric and test code for it

--- autorag/evaluate/metric/generation.py
+++ autorag/evaluate/metric/generation.py
@@ -1,14 +1,18 @@
 import functools
-from typing import List
+from typing import List, Optional
 
 import evaluate
 import pandas as pd
 import sacrebleu
+from llama_index.core.embeddings.base import BaseEmbedding
+
+from autorag import embedding_models
+from autorag.evaluate.metric.util import calculate_cosine_similarity
 
 
 def generation_metric(func):
     @functools.wraps(func)
-    def wrapper(generation_gt: List[List[str]], generations: List[str]) -> List[float]:
+    def wrapper(generation_gt: List[List[str]], generations: List[str], **kwargs) -> List[float]:
         """
         Compute generation metric.
 
@@ -16,11 +20,12 @@ def generation_metric(func):
             Must be 2-d list of string.
             Because it can be a multiple ground truth.
         :param generations: A list of generations that LLM generated.
+        :param kwargs: The additional arguments for metric function.
         :return: A list of computed metric scores.
         """
         # make generation_gt and generations to pd dataframe
         df = pd.DataFrame({'gt': generation_gt, 'pred': generations})
-        df[func.__name__] = df.swifter.apply(lambda x: func(x['gt'], x['pred']), axis=1)
+        df[func.__name__] = df.swifter.apply(lambda x: func(x['gt'], x['pred'], **kwargs), axis=1)
         return df[func.__name__].tolist()
 
     return wrapper
@@ -82,3 +87,28 @@ def rouge(generation_gt: List[List[str]], generations: List[str]) -> List[float]
     """
     rouge_instance = evaluate.load("rouge")
     return huggingface_evaluate(rouge_instance, 'rougeL', generation_gt, generations)
+
+
+@generation_metric
+def sem_score(generation_gt: List[str], pred: str, embedding_model: Optional[BaseEmbedding] = None) -> float:
+    """
+    Compute sem score between generation gt and pred with cosine similarity.
+
+    :param generation_gt: A list of ground truth.
+        Must be list of string.
+        It will get the max of cosine similarity between generation_gt and pred.
+    :param pred: Model prediction.
+    :param embedding_model: Embedding model to use for compute cosine similarity.
+        Default is all-mpnet-base-v2 embedding model.
+        The paper used this embedding model.
+    :return: Sem score between generation_gt and pred.
+    """
+    if embedding_model is None:
+        embedding_model = embedding_models['huggingface_all_mpnet_base_v2']
+
+    gt_embeddings = embedding_model.get_text_embedding_batch(generation_gt)
+    pred_embedding = embedding_model.get_text_embedding(pred)
+
+    # calculate cosine similarity
+    similarity_scores: List[float] = list(map(lambda x: calculate_cosine_similarity(x, pred_embedding), gt_embeddings))
+    return max(similarity_scores)
---
+++ autorag/evaluate/metric/util.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def calculate_cosine_similarity(a, b):
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    similarity = dot_product / (norm_a * norm_b)
+    return similarity
