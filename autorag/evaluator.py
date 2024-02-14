9a2d25627d2321f3fc44004b64a57b45552a17a3
jeffrey <vkefhdl1@gmail.com>
2024-02-14 19:57:00 +0000

    add project_dir option to evaluator and runner cli

--- autorag/evaluator.py
+++ autorag/evaluator.py
@@ -185,12 +185,13 @@ def cli():
 @click.option('--config', '-c', help='Path to config yaml file. Must be yaml or yml file.', type=str)
 @click.option('--qa_data_path', help='Path to QA dataset. Must be parquet file.', type=str)
 @click.option('--corpus_data_path', help='Path to corpus dataset. Must be parquet file.', type=str)
-def evaluate(config, qa_data_path, corpus_data_path):
+@click.option('--project_dir', help='Path to project directory.', type=str, default=None)
+def evaluate(config, qa_data_path, corpus_data_path, project_dir):
     if not config.endswith('.yaml') and not config.endswith('.yml'):
         raise ValueError(f"Config file {config} is not a parquet file.")
     if not os.path.exists(config):
         raise ValueError(f"Config file {config} does not exist.")
-    evaluator = Evaluator(qa_data_path, corpus_data_path)
+    evaluator = Evaluator(qa_data_path, corpus_data_path, project_dir=project_dir)
     evaluator.start_trial(config)
     logger.info('Evaluation complete.')
 
@@ -199,8 +200,9 @@ def evaluate(config, qa_data_path, corpus_data_path):
 @click.option('--config_path', type=str, help='Path to extracted config yaml file.')
 @click.option('--host', type=str, default='0.0.0.0', help='Host address')
 @click.option('--port', type=int, default=8000, help='Port number')
-def run_api(config_path, host, port):
-    runner = Runner.from_yaml(config_path)
+@click.option('--project_dir', help='Path to project directory.', type=str, default=None)
+def run_api(config_path, host, port, project_dir):
+    runner = Runner.from_yaml(config_path, project_dir=project_dir)
     logger.info(f"Running API server at {host}:{port}...")
     runner.run_api_server(host, port)
 
--- tests/autorag/test_evaluator.py
+++ tests/autorag/test_evaluator.py
@@ -125,19 +125,21 @@ def test_start_trial(evaluator):
     assert trial_summary_df['best_execution_time'][0] > 0
 
 
-def test_evaluator_cli(evaluator):
-    result = subprocess.run(['autorag', 'evaluate', '--config', os.path.join(resource_dir, 'simple.yaml'),
-                             '--qa_data_path', os.path.join(resource_dir, 'qa_data_sample.parquet'),
-                             '--corpus_data_path', os.path.join(resource_dir, 'corpus_data_sample.parquet')])
-    assert result.returncode == 0
-    # check if the files are created
-    assert os.path.exists(os.path.join(os.getcwd(), '0'))
-    assert os.path.exists(os.path.join(os.getcwd(), 'data'))
-    assert os.path.exists(os.path.join(os.getcwd(), 'resources'))
-    assert os.path.exists(os.path.join(os.getcwd(), 'trial.json'))
-    assert os.path.exists(os.path.join(os.getcwd(), '0', 'retrieve_node_line'))
-    assert os.path.exists(os.path.join(os.getcwd(), '0', 'retrieve_node_line', 'retrieval'))
-    assert os.path.exists(os.path.join(os.getcwd(), '0', 'retrieve_node_line', 'retrieval', '0.parquet'))
+def test_evaluator_cli():
+    with tempfile.TemporaryDirectory() as project_dir:
+        result = subprocess.run(['autorag', 'evaluate', '--config', os.path.join(resource_dir, 'simple.yaml'),
+                                 '--qa_data_path', os.path.join(resource_dir, 'qa_data_sample.parquet'),
+                                 '--corpus_data_path', os.path.join(resource_dir, 'corpus_data_sample.parquet'),
+                                 '--project_dir', project_dir])
+        assert result.returncode == 0
+        # check if the files are created
+        assert os.path.exists(os.path.join(project_dir, '0'))
+        assert os.path.exists(os.path.join(project_dir, 'data'))
+        assert os.path.exists(os.path.join(project_dir, 'resources'))
+        assert os.path.exists(os.path.join(project_dir, 'trial.json'))
+        assert os.path.exists(os.path.join(project_dir, '0', 'retrieve_node_line'))
+        assert os.path.exists(os.path.join(project_dir, '0', 'retrieve_node_line', 'retrieval'))
+        assert os.path.exists(os.path.join(project_dir, '0', 'retrieve_node_line', 'retrieval', '0.parquet'))
 
 
 def start_trial_full(evaluator):
