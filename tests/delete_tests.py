0adf4fd95cf941b5c54079496f83fc07f0388252
jeffrey <vkefhdl1@gmail.com>
2024-02-18 15:22:09 +0000

    enable logging please work

--- tests/delete_tests.py
+++ tests/delete_tests.py
@@ -5,8 +5,12 @@ from distutils.sysconfig import get_python_lib
 
 def main():
     tests_dir = os.path.join(get_python_lib(), 'tests')
+    print(f"Github Actions: {os.getenv('GITHUB_ACTIONS')}")
+    print(get_python_lib())
     if os.path.exists(tests_dir):
         shutil.rmtree(tests_dir)
+    else:
+        print("Directory does not exist")
 
 
 if __name__ == "__main__":
