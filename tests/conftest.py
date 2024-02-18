8809fb6b1ec875562840b5f286e3aadac38bce28
jeffrey <vkefhdl1@gmail.com>
2024-02-18 11:45:58 +0000

    add nltk punkt for testing tree_summarize

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,11 @@ import os
 import shutil
 from distutils.sysconfig import get_python_lib
 
+import nltk
+
 
 def pytest_sessionstart():
+    nltk.download('punkt')  # Download for testing tree_summarize as an async process
     tests_dir = os.path.join(get_python_lib(), 'tests')
     if os.path.exists(tests_dir):
         shutil.rmtree(tests_dir)
