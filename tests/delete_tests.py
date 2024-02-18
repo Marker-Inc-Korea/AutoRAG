0188fc5dc3f852d4761626fd2c1957533bbfac2f
jeffrey <vkefhdl1@gmail.com>
2024-02-18 15:40:14 +0000

    test is_ubuntu

--- tests/delete_tests.py
+++ tests/delete_tests.py
@@ -2,10 +2,27 @@ import os
 import shutil
 import sysconfig
 
+import torch.cuda
+
+
+def is_ubuntu() -> bool:
+    try:
+        with open("/etc/os-release") as f:
+            for line in f:
+                if line.startswith("NAME") and "Ubuntu" in line:
+                    return True
+    except FileNotFoundError:
+        pass
+    return False
+
+
+def is_github_action() -> bool:
+    return is_ubuntu() and not torch.cuda.is_available()
+
 
 def main():
     tests_dir = os.path.join(sysconfig.get_path('purelib'), 'tests')
-    print(f"Github Actions: {os.getenv('CI')}")
+    print(is_github_action())
     print(sysconfig.get_path('purelib'))
     if os.path.exists(tests_dir):
         shutil.rmtree(tests_dir)
