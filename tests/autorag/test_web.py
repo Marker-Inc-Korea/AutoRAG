819c1bb21cec5a273ec8d05f789092e06aa12112
kimbwook <bwook00@naver.com>
2024-03-05 16:48:23 +0000

    Add test_web.py

--- tests/autorag/test_web.py
+++ tests/autorag/test_web.py
@@ -2,10 +2,20 @@ import os
 import pathlib
 import subprocess
 
+import pytest
+
 root_dir = pathlib.PurePath(os.path.dirname(os.path.realpath(__file__))).parent
 resource_dir = os.path.join(root_dir, 'resources')
 
 
-def test_web_cli_yaml():
-    result = subprocess.run(
-        ['autorag', 'run_web', '--yaml_path', 'test/path/test.yaml', '--project_dir', 'test/path'])
+@pytest.mark.skip(reason="The only way to test it is to run it directly on the web and see if it works."
+                         "If you want to test it, clear this line and run it.")
+def test_web_cli_yaml_project_dir():
+    subprocess.run(['autorag', 'run_web', '--yaml_path', '../../sample_config/best.yaml',
+                    '--project_dir', '../resources/result_project'])
+
+
+@pytest.mark.skip(reason="The only way to test it is to run it directly on the web and see if it works."
+                         "If you want to test it, clear this line and run it.")
+def test_web_cli_trial():
+    subprocess.run(['autorag', 'run_web', '--trial_path', '../resources/result_project/0'])
