b48aa303ee818bfe339e777ed666b661055625f2
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-03-08 23:59:08 +0000

    Add vllm Module at generator (#216)
    * add vllm generator module
    It supports token id and log probs!
    
    * add vllm at __init__.py
    
    * lazy import vllm
    
    * add vllm test
    
    * run vllm as node
    
    * fix code to logprobs
    
    * enable logprob
    
    * fix error
    
    * delete logging and refactor test vllm
    
    * add vllm at support_modules
    
    * skip vllm test
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>

--- autorag/nodes/generator/__init__.py
+++ autorag/nodes/generator/__init__.py
@@ -1,1 +1,2 @@
 from .llama_index_llm import llama_index_llm
+from .vllm import vllm
---
+++ tests/autorag/nodes/generator/test_generator_base.py
@@ -0,0 +1,23 @@
+prompts = [
+    "Who is the strongest Avenger?",
+    "Who is the best soccer player in the world?",
+    "Who is the president of the United States?",
+]
+
+
+def check_generated_texts(generated_texts):
+    assert len(generated_texts) == len(prompts)
+    assert isinstance(generated_texts[0], str)
+    assert all(bool(text) is True for text in generated_texts)
+
+
+def check_generated_tokens(tokens):
+    assert len(tokens) == len(prompts)
+    assert isinstance(tokens[0], list)
+    assert isinstance(tokens[0][0], int)
+
+
+def check_generated_log_probs(log_probs):
+    assert len(log_probs) == len(prompts)
+    assert isinstance(log_probs[0], list)
+    assert isinstance(log_probs[0][0], float)
