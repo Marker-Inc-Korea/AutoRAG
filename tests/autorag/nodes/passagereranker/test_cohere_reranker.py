a00cbed20f7b23df11001abcf6a7deac7549ade4
jeffrey <vkefhdl1@gmail.com>
2024-03-15 18:19:28 +0900

    add cohere reranker to support.py

--- tests/autorag/nodes/passagereranker/test_cohere_reranker.py
+++ tests/autorag/nodes/passagereranker/test_cohere_reranker.py
@@ -1,4 +1,4 @@
-from autorag.nodes.passagereranker.cohere import cohere_reranker
+from autorag.nodes.passagereranker import cohere_reranker
 
 from tests.autorag.nodes.passagereranker.test_passage_reranker_base import queries_example, contents_example, \
     scores_example, ids_example, base_reranker_test, project_dir, previous_result, base_reranker_node_test
