6e278e6221e45c3847b81e48fd4c8477700b1695
jeffrey <vkefhdl1@gmail.com>
2024-04-04 16:14:15 +0900

    change order of modules in support.py for readability

--- autorag/support.py
+++ autorag/support.py
@@ -14,24 +14,30 @@ def dynamically_find_function(key: str, target_dict: Dict) -> Callable:
 
 def get_support_modules(module_name: str) -> Callable:
     support_modules = {
+        # query_expansion
         'query_decompose': ('autorag.nodes.queryexpansion', 'query_decompose'),
         'hyde': ('autorag.nodes.queryexpansion', 'hyde'),
+        'pass_query_expansion': ('autorag.nodes.queryexpansion', 'pass_query_expansion'),
+        # retrieval
         'bm25': ('autorag.nodes.retrieval', 'bm25'),
         'vectordb': ('autorag.nodes.retrieval', 'vectordb'),
-        'fstring': ('autorag.nodes.promptmaker', 'fstring'),
-        'llama_index_llm': ('autorag.nodes.generator', 'llama_index_llm'),
-        'vllm': ('autorag.nodes.generator', 'vllm'),
-        'tree_summarize': ('autorag.nodes.passagecompressor', 'tree_summarize'),
+        'hybrid_rrf': ('autorag.nodes.retrieval', 'hybrid_rrf'),
+        'hybrid_cc': ('autorag.nodes.retrieval', 'hybrid_cc'),
+        # passage_reranker
         'monot5': ('autorag.nodes.passagereranker', 'monot5'),
         'tart': ('autorag.nodes.passagereranker', 'tart'),
         'upr': ('autorag.nodes.passagereranker', 'upr'),
-        'hybrid_rrf': ('autorag.nodes.retrieval', 'hybrid_rrf'),
-        'hybrid_cc': ('autorag.nodes.retrieval', 'hybrid_cc'),
         'koreranker': ('autorag.nodes.passagereranker', 'koreranker'),
         'pass_reranker': ('autorag.nodes.passagereranker', 'pass_reranker'),
-        'pass_query_expansion': ('autorag.nodes.queryexpansion', 'pass_query_expansion'),
-        'pass_compressor': ('autorag.nodes.passagecompressor', 'pass_compressor'),
         'cohere_reranker': ('autorag.nodes.passagereranker', 'cohere_reranker'),
+        # passage_compressor
+        'tree_summarize': ('autorag.nodes.passagecompressor', 'tree_summarize'),
+        'pass_compressor': ('autorag.nodes.passagecompressor', 'pass_compressor'),
+        # prompt_maker
+        'fstring': ('autorag.nodes.promptmaker', 'fstring'),
+        # generator
+        'llama_index_llm': ('autorag.nodes.generator', 'llama_index_llm'),
+        'vllm': ('autorag.nodes.generator', 'vllm'),
     }
     return dynamically_find_function(module_name, support_modules)
 
