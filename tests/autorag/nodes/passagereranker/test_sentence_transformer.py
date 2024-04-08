ab938b58a652461122454927149d62f493ec9e5a
kimbwook <bwook00@naver.com>
2024-04-08 15:34:21 +0900

    add reranker at test function name

--- tests/autorag/nodes/passagereranker/test_sentence_transformer.py
+++ tests/autorag/nodes/passagereranker/test_sentence_transformer.py
@@ -4,7 +4,7 @@ from tests.autorag.nodes.passagereranker.test_passage_reranker_base import queri
     scores_example, ids_example, base_reranker_test, project_dir, previous_result, base_reranker_node_test
 
 
-def test_sentence_transformer():
+def test_sentence_transformer_reranker():
     top_k = 3
     original_sentence_transformer = sentence_transformer_reranker.__wrapped__
     contents_result, id_result, score_result \
@@ -12,7 +12,7 @@ def test_sentence_transformer():
     base_reranker_test(contents_result, id_result, score_result, top_k)
 
 
-def test_sentence_transformer_node():
+def test_sentence_transformer_reranker_node():
     top_k = 1
     result_df = sentence_transformer_reranker(project_dir=project_dir, previous_result=previous_result, top_k=top_k)
     base_reranker_node_test(result_df, top_k)
