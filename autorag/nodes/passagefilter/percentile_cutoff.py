1266c1156d5cfd0118edad31937bb4c24aa57aa7
kimbwook <bwook00@naver.com>
2024-04-11 21:34:06 +0900

    Implement percentile_cutoff.py

---
+++ autorag/nodes/passagefilter/percentile_cutoff.py
@@ -0,0 +1,90 @@
+import itertools
+from typing import List, Tuple, Optional
+
+import numpy as np
+import torch.cuda
+
+from autorag import embedding_models
+from autorag.evaluate.metric.util import calculate_cosine_similarity
+from autorag.nodes.passagefilter.base import passage_filter_node
+from autorag.utils.util import reconstruct_list
+
+
+@passage_filter_node
+def similarity_percentile_cutoff(queries: List[str], contents_list: List[List[str]],
+                                 scores_list: List[List[float]], ids_list: List[List[str]],
+                                 percentile: float, embedding_model: Optional[str] = None,
+                                 batch: int = 128,
+                                 ) -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
+    """
+    Re-calculate each content's similarity with the query and filter out the contents that are below the threshold.
+    If all contents are filtered, keep the only one highest similarity content.
+    This is a filter and does not override scores.
+    The output of scores is not coming from query-content similarity.
+
+    :param queries: The list of queries to use for filtering
+    :param contents_list: The list of lists of contents to filter
+    :param scores_list: The list of lists of scores retrieved
+    :param ids_list: The list of lists of ids retrieved
+    :param percentile: The percentile to cut off
+    :param embedding_model: The embedding model to use for calculating similarity
+        Default is OpenAIEmbedding.
+    :param batch: The number of queries to be processed in a batch
+        Default is 128.
+    :return: Tuple of lists containing the filtered contents, ids, and scores
+    """
+    if embedding_model is None:
+        embedding_model = embedding_models['openai']
+    else:
+        embedding_model = embedding_models[embedding_model]
+
+    # Embedding using batch
+    embedding_model.embed_batch_size = batch
+    query_embeddings = embedding_model.get_text_embedding_batch(queries)
+
+    content_lengths = list(map(len, contents_list))
+    content_embeddings_flatten = embedding_model.get_text_embedding_batch(list(
+        itertools.chain.from_iterable(contents_list)))
+    content_embeddings = reconstruct_list(content_embeddings_flatten, content_lengths)
+
+    results = list(map(lambda x: similarity_percentile_cutoff_pure(x[0], x[1], x[2], x[3], percentile),
+                       zip(query_embeddings, content_embeddings, contents_list, ids_list)))
+
+    remain_content_list = list(map(lambda x: x[0], results))
+    remain_scores_list = list(map(lambda x: x[1], results))
+    remain_ids_list = list(map(lambda x: x[2], results))
+
+    del embedding_model
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+
+    return remain_content_list, remain_ids_list, remain_scores_list
+
+
+def similarity_percentile_cutoff_pure(query_embedding: str,
+                                      content_embeddings: List[List[float]],
+                                      content_list: List[str],
+                                      ids_list: List[str],
+                                      percentile: float) -> Tuple[List[str], List[str], List[float]]:
+    """
+    Return indices that have to remain.
+    Return at least one index if there is nothing to remain.
+
+    :param query_embedding: Query embedding
+    :param content_embeddings: Each content embedding
+    :param content_list: Each content
+    :param ids_list: Each id
+    :param percentile: The percentile to cut off
+    :return: Indices to remain at the contents
+    """
+    num_top_k = int(len(content_embeddings) * percentile)
+
+    similarities = np.array(list(map(lambda x: calculate_cosine_similarity(query_embedding, x),
+                                     content_embeddings))).tolist()
+
+    content_id_score = list(zip(ids_list, content_list, similarities))
+
+    sorted_content_id_score = sorted(content_id_score, key=lambda x: x[2], reverse=True)[:num_top_k]
+
+    content_result, id_result, score_result = zip(*sorted_content_id_score)
+    return list(content_result), list(id_result), list(score_result)
