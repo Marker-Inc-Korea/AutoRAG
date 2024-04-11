c974058593990903f1d7c0ea36e15a6d97a769cf
kimbwook <bwook00@naver.com>
2024-04-11 16:38:42 +0900

    Add more test case

--- autorag/nodes/passagefilter/time.py
+++ autorag/nodes/passagefilter/time.py
@@ -1,7 +1,10 @@
+import logging
 from datetime import datetime
 from typing import List, Tuple
 
 from autorag.nodes.passagefilter.base import passage_filter_node
+
+logger = logging.getLogger("AutoRAG")
 
 
 @passage_filter_node
@@ -10,17 +13,37 @@ def time_filter(contents_list: List[List[str]],
                 time_list: List[List[datetime]],
                 threshold: str,
                 ) -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
-    def sort_row(contents, scores, ids, time, datetime_threshold):
-        combined_filtered = [(content, score, id, t) for content, score, id, t in zip(contents, scores, ids, time) if
-                             t >= datetime_threshold]
+    """
+    Filter out the contents that are below the threshold datetime.
+    If all contents are filtered, keep the only one recency content.
+    If the threshold date format is incorrect, return the original contents.
+
+    :param contents_list: The list of lists of contents to filter
+    :param scores_list: The list of lists of scores retrieved
+    :param ids_list: The list of lists of ids retrieved
+    :param time_list: The list of lists of datetime retrieved
+    :param threshold: The threshold to cut off
+    :return: Tuple of lists containing the filtered contents, ids, and scores
+    """
+
+    def sort_row(contents, scores, ids, time, _datetime_threshold):
+        combined = list(zip(contents, scores, ids, time))
+        combined_filtered = [item for item in combined if item[3] >= _datetime_threshold]
+
         if combined_filtered:
             remain_contents, remain_scores, remain_ids, _ = zip(*combined_filtered)
-            return list(remain_contents), list(remain_ids), list(remain_scores)
         else:
-            return [], [], []
+            combined.sort(key=lambda x: x[3], reverse=True)
+            remain_contents, remain_scores, remain_ids, _ = zip(*combined[:1])
 
-    # threshold 유효성 검사 추가하기
-    datetime_threshold = datetime.strptime(threshold, "%Y-%m-%d")
+        return list(remain_contents), list(remain_ids), list(remain_scores)
+
+    try:
+        datetime_threshold = datetime.strptime(threshold, "%Y-%m-%d")
+    except ValueError:
+        logger.info("threshold date format is incorrect, should be YYYY-MM-DD")
+        return contents_list, ids_list, scores_list
+
     remain_contents_list, remain_ids_list, remain_scores_list = zip(
         *map(sort_row, contents_list, scores_list, ids_list, time_list, [datetime_threshold] * len(contents_list)))
 
--- tests/autorag/nodes/passagefilter/test_time_filter.py
+++ tests/autorag/nodes/passagefilter/test_time_filter.py
@@ -1,20 +1,46 @@
 from autorag.nodes.passagefilter import time_filter
 
-from tests.autorag.nodes.passagefilter.test_passage_filter_base import queries_example, contents_example, time_list, \
-    ids_example, scores_example, base_passage_filter_test, base_passage_filter_node_test, project_dir_with_corpus, \
+from tests.autorag.nodes.passagefilter.test_passage_filter_base import contents_example, time_list, ids_example, \
+    scores_example, base_passage_filter_test, base_passage_filter_node_test, project_dir_with_corpus, \
     previous_result
 
 
 def test_time_filter():
     original_time_filter = time_filter.__wrapped__
     contents_result, id_result, score_result = original_time_filter \
-        (queries_example, contents_example, scores_example, ids_example, time_list, threshold="2021-06-30")
+        (contents_example, scores_example, ids_example, time_list, threshold="2021-06-30")
     assert id_result[0] == [ids_example[0][3]]
     assert id_result[1] == [ids_example[1][1], ids_example[1][2]]
     assert contents_result[0] == [contents_example[0][3]]
     assert contents_result[1] == [contents_example[1][1], contents_example[1][2]]
     assert score_result[0] == [0.5]
     assert score_result[1] == [0.2, 0.7]
+    base_passage_filter_test(contents_result, id_result, score_result)
+
+
+def test_time_filter_all_filtered():
+    original_time_filter = time_filter.__wrapped__
+    contents_result, id_result, score_result = original_time_filter \
+        (contents_example, scores_example, ids_example, time_list, threshold="2040-06-30")
+    assert id_result[0] == [ids_example[0][3]]
+    assert id_result[1] == [ids_example[1][2]]
+    assert contents_result[0] == [contents_example[0][3]]
+    assert contents_result[1] == [contents_example[1][2]]
+    assert score_result[0] == [0.5]
+    assert score_result[1] == [0.7]
+    base_passage_filter_test(contents_result, id_result, score_result)
+
+
+def test_time_filter_wrong_threshold():
+    original_time_filter = time_filter.__wrapped__
+    contents_result, id_result, score_result = original_time_filter \
+        (contents_example, scores_example, ids_example, time_list, threshold="havertz")
+    assert id_result[0] == ids_example[0]
+    assert id_result[1] == ids_example[1]
+    assert contents_result[0] == contents_example[0]
+    assert contents_result[1] == contents_example[1]
+    assert score_result[0] == scores_example[0]
+    assert score_result[1] == scores_example[1]
     base_passage_filter_test(contents_result, id_result, score_result)
 
 
