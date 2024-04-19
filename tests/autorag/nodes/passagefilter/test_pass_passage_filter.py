5ecddc5ca3f9aa6ae56affd938427565cb312499
kimbwook <bwook00@naver.com>
2024-04-19 14:15:58 +0900

    delete blank line

--- tests/autorag/nodes/passagefilter/test_pass_passage_filter.py
+++ tests/autorag/nodes/passagefilter/test_pass_passage_filter.py
@@ -4,10 +4,7 @@ from tests.autorag.nodes.passagefilter.test_passage_filter_base import project_d
 
 
 def test_pass_passage_filter():
-    result_df = pass_passage_filter(
-        project_dir=project_dir,
-        previous_result=previous_result,
-    )
+    result_df = pass_passage_filter(project_dir=project_dir, previous_result=previous_result)
     assert all([column_name in result_df.columns for column_name in
                 ["retrieved_contents", "retrieved_ids", "retrieve_scores"]])
     assert result_df["retrieved_contents"].tolist() == contents_example
