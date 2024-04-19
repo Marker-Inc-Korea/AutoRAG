e2f1c36a6faf50beadbae75b86951e948227a28f
kimbwook <bwook00@naver.com>
2024-04-19 14:20:23 +0900

    change function name

--- tests/autorag/nodes/passageaugmenter/test_pass_passage_augmenter.py
+++ tests/autorag/nodes/passageaugmenter/test_pass_passage_augmenter.py
@@ -3,13 +3,13 @@ from autorag.nodes.passageaugmenter import pass_passage_augmenter
 from tests.autorag.nodes.passageaugmenter.test_base_passage_augmenter import project_dir, previous_result, corpus_data
 
 
-def test_prev_next_augmenter_node():
-    result_df = pass_passage_augmenter(project_dir=project_dir, previous_result=previous_result, mode='next')
+def test_pass_passage_augmenter():
+    result_df = pass_passage_augmenter(project_dir=project_dir, previous_result=previous_result)
     contents = result_df["retrieved_contents"].tolist()
     ids = result_df["retrieved_ids"].tolist()
     scores = result_df["retrieve_scores"].tolist()
     assert len(contents) == len(ids) == len(scores) == 2
-    assert len(contents[0]) == len(ids[0]) == len(scores[0]) == 4
+    assert len(contents[0]) == len(ids[0]) == len(scores[0]) == 2
     for content_list, id_list, score_list in zip(contents, ids, scores):
         for i, (content, _id, score) in enumerate(zip(content_list, id_list, score_list)):
             assert isinstance(content, str)
