7c0c84840f36621d9d5ac3a5924dad1812441606
Bwook (Byoungwook) Kim <bwook00@naver.com>
2024-04-24 23:45:02 +0900

    [Hotfix] resolve colbert, flag embedding reranker batch error  (#366)
    * hothix tart error
    
    * add batch one test code tart, sentence transformer, monot5
    
    * add batch test code flag and colbert
    
    * add batch test code at cohere jina rankgpt
    
    * delete batch size 1 limit at flag embedding
    
    * rollback colbert
    
    * batch one cohere jina rankgpt
    
    ---------
    
    Co-authored-by: Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>

--- tests/autorag/nodes/passagereranker/test_flag_embedding.py
+++ tests/autorag/nodes/passagereranker/test_flag_embedding.py
@@ -16,6 +16,26 @@ def test_flag_embedding_reranker():
 
 
 @pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
+def test_flag_embedding_reranker_batch_one():
+    top_k = 3
+    batch = 1
+    original_flag_embedding = flag_embedding_reranker.__wrapped__
+    contents_result, id_result, score_result \
+        = original_flag_embedding(queries_example, contents_example, scores_example, ids_example, top_k, batch)
+    base_reranker_test(contents_result, id_result, score_result, top_k)
+
+
+@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
+def test_flag_embedding_reranker_batch():
+    top_k = 3
+    batch = 2
+    original_flag_embedding = flag_embedding_reranker.__wrapped__
+    contents_result, id_result, score_result \
+        = original_flag_embedding(queries_example, contents_example, scores_example, ids_example, top_k, batch)
+    base_reranker_test(contents_result, id_result, score_result, top_k)
+
+
+@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
 def test_flag_embedding_reranker_node():
     top_k = 1
     result_df = flag_embedding_reranker(project_dir=project_dir, previous_result=previous_result, top_k=top_k)
--- tests/autorag/nodes/passagereranker/test_flag_embedding_llm.py
+++ tests/autorag/nodes/passagereranker/test_flag_embedding_llm.py
@@ -17,6 +17,28 @@ def test_flag_embedding_llm_reranker():
 
 
 @pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
+def test_flag_embedding_llm_reranker_batch_one():
+    top_k = 3
+    batch = 1
+    original_flag_embedding_llm = flag_embedding_llm_reranker.__wrapped__
+    contents_result, id_result, score_result \
+        = original_flag_embedding_llm(queries_example, contents_example, scores_example, ids_example, top_k, batch,
+                                      use_fp16=True)
+    base_reranker_test(contents_result, id_result, score_result, top_k)
+
+
+@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
+def test_flag_embedding_llm_reranker_batch():
+    top_k = 3
+    batch = 2
+    original_flag_embedding_llm = flag_embedding_llm_reranker.__wrapped__
+    contents_result, id_result, score_result \
+        = original_flag_embedding_llm(queries_example, contents_example, scores_example, ids_example, top_k, batch,
+                                      use_fp16=True)
+    base_reranker_test(contents_result, id_result, score_result, top_k)
+
+
+@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
 def test_flag_embedding_llm_reranker_node():
     top_k = 1
     result_df = flag_embedding_llm_reranker(project_dir=project_dir, previous_result=previous_result, top_k=top_k)
