c310490fb1fcc1665aaea651fe8073a4f00cc17f
kimbwook <bwook00@naver.com>
2024-04-25 23:47:24 +0900

    add empty test code at fetch content

--- tests/autorag/utils/test_util.py
+++ tests/autorag/utils/test_util.py
@@ -70,6 +70,10 @@ def test_fetch_contents():
                                  {'last_modified_datetime': datetime(2022, 1, 1, 0, 0, 0)}]
     assert find_metadatas[1] == [{'last_modified_datetime': datetime(2022, 1, 2, 0, 0, 0)}]
 
+    find_empty = fetch_contents(corpus_data, [[], ['doc2']])
+    assert find_empty[0] == []
+    assert find_empty[1] == ['banana']
+
 
 def test_load_summary_file(summary_path):
     with pytest.raises(ValueError):
