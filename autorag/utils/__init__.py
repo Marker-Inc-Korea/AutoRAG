97476b483a589731a781366f260bf999c20346b5
Bwook (Byoungwook) Kim <bwook00@naver.com>
2024-04-27 15:28:05 +0900

    Add test code if QA data was created from a corpus (#382)
    * add validate_qa_from_corpus_dataset
    
    * add validate_qa_from_corpus_dataset at evaluator class init

--- autorag/utils/__init__.py
+++ autorag/utils/__init__.py
@@ -1,2 +1,3 @@
-from .preprocess import validate_qa_dataset, validate_corpus_dataset, cast_qa_dataset, cast_corpus_dataset
+from .preprocess import (validate_qa_dataset, validate_corpus_dataset, cast_qa_dataset, cast_corpus_dataset,
+                         validate_qa_from_corpus_dataset)
 from .util import fetch_contents, result_to_dataframe, sort_by_scores
