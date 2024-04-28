e96b5ae14db1ee890f54fd7f2a6d526b2139ca88
Bwook (Byoungwook) Kim <bwook00@naver.com>
2024-04-28 14:01:31 +0900

    Parallel processing at UPR reranker (#334)
    * update upr
    
    * update upr 2
    
    * update upr 3
    
    * update upr 4
    
    * update upr 4
    
    * apply futeres
    
    * apply futeres
    
    * apply from multiprocessing import Pool
    
    * apply from multiprocessing import Pool
    
    * merge #308
    
    * update upr
    
    * update upr 2
    
    * update upr 3
    
    * apply torch.multiprocessing for gpu process
    
    * add one batch test at upr reranker test code
    
    * change model to cuda device at the start
    
    * process upr well
    
    * use ray for processing upr parallel
    
    * change prompt_token to the given device
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>

--- tests/autorag/nodes/passagereranker/test_upr.py
+++ tests/autorag/nodes/passagereranker/test_upr.py
@@ -12,11 +12,11 @@ def test_upr():
     original_upr = upr.__wrapped__
     contents_result, id_result, score_result \
         = original_upr(queries_example, contents_example, scores_example, ids_example, top_k)
-    base_reranker_test(contents_result, id_result, score_result, top_k)
+    base_reranker_test(contents_result, id_result, score_result, top_k, descending=False)
 
 
 @pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
 def test_upr_node():
-    top_k = 1
+    top_k = 3
     result_df = upr(project_dir=project_dir, previous_result=previous_result, top_k=top_k)
-    base_reranker_node_test(result_df, top_k)
+    base_reranker_node_test(result_df, top_k, descending=False)
