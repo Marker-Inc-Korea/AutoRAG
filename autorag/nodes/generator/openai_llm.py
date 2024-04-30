200255d0f250941984a333e37271f28b9d9bd5af
jeffrey <vkefhdl1@gmail.com>
2024-04-30 16:42:10 +0900

    test code for openai_llm and resolve bugs

--- autorag/nodes/generator/openai_llm.py
+++ autorag/nodes/generator/openai_llm.py
@@ -14,7 +14,7 @@ logger = logging.getLogger("AutoRAG")
 
 MAX_TOKEN_DICT = {  # model name : token limit
     'gpt-4-turbo': 128_000,
-    'gpt-4-turbo-2024-04-09	': 128_000,
+    'gpt-4-turbo-2024-04-09': 128_000,
     'gpt-4-turbo-preview': 128_000,
     'gpt-4-0125-preview': 128_000,
     'gpt-4-1106-preview': 128_000,
@@ -35,10 +35,10 @@ MAX_TOKEN_DICT = {  # model name : token limit
 
 
 @generator_node
-def openai(prompts: List[str], model: str = "gpt-3.5-turbo", batch: int = 16,
-           truncate: bool = True,
-           api_key: str = None,
-           **kwargs) -> \
+def openai_llm(prompts: List[str], llm: str = "gpt-3.5-turbo", batch: int = 16,
+               truncate: bool = True,
+               api_key: str = None,
+               **kwargs) -> \
         Tuple[List[str], List[List[int]], List[List[float]]]:
     """
     OpenAI generator module.
@@ -46,7 +46,7 @@ def openai(prompts: List[str], model: str = "gpt-3.5-turbo", batch: int = 16,
     It returns real token ids and log probs, so you must use this for using token ids and log probs.
 
     :param prompts: A list of prompts.
-    :param model: A model name for openai.
+    :param llm: A model name for openai.
         Default is gpt-3.5-turbo.
     :param batch: Batch size for openai api call.
         If you get API limit errors, you should lower the batch size.
@@ -67,17 +67,17 @@ def openai(prompts: List[str], model: str = "gpt-3.5-turbo", batch: int = 16,
             raise ValueError("OPENAI_API_KEY does not set. "
                              "Please set env variable OPENAI_API_KEY or pass api_key parameter to openai module.")
 
-    tokenizer = tiktoken.encoding_for_model(model)
+    tokenizer = tiktoken.encoding_for_model(llm)
     if truncate:
-        max_token_size = MAX_TOKEN_DICT.get('model')
+        max_token_size = MAX_TOKEN_DICT.get(llm) - 7  # because of chat token usage
         if max_token_size is None:
-            raise ValueError(f"Model {model} does not supported. "
+            raise ValueError(f"Model {llm} does not supported. "
                              f"Please select the model between {list(MAX_TOKEN_DICT.keys())}")
         prompts = list(map(lambda prompt: truncate_by_token(prompt, tokenizer, max_token_size), prompts))
 
     client = AsyncOpenAI(api_key=api_key)
     loop = asyncio.get_event_loop()
-    tasks = [get_result(prompt, client, model, tokenizer, **kwargs) for prompt in prompts]
+    tasks = [get_result(prompt, client, llm, tokenizer, **kwargs) for prompt in prompts]
     result = loop.run_until_complete(process_batch(tasks, batch))
     answer_result = list(map(lambda x: x[0], result))
     token_result = list(map(lambda x: x[1], result))
@@ -86,9 +86,11 @@ def openai(prompts: List[str], model: str = "gpt-3.5-turbo", batch: int = 16,
 
 
 async def get_result(prompt: str, client: AsyncOpenAI, model: str, tokenizer: Encoding, **kwargs):
-    if kwargs.pop('logprobs') is not None:
+    if kwargs.get('logprobs') is not None:
+        kwargs.pop('logprobs')
         logger.warning("parameter logprob does not effective. It always set to True.")
-    if kwargs.pop('n') is not None:
+    if kwargs.get('n') is not None:
+        kwargs.pop('n')
         logger.warning("parameter n does not effective. It always set to 1.")
 
     response = await client.chat.completions.create(
@@ -102,12 +104,12 @@ async def get_result(prompt: str, client: AsyncOpenAI, model: str, tokenizer: En
     )
     choice = response.choices[0]
     answer = choice.message.content
-    logprobs = list(map(lambda x: x['logprob'], choice.logprobs.content))
-    tokens = tokenizer.encode(answer)
+    logprobs = list(map(lambda x: x.logprob, choice.logprobs.content))
+    tokens = tokenizer.encode(answer, allowed_special='all')
     assert len(tokens) == len(logprobs), "tokens and logprobs size is different."
     return answer, tokens, logprobs
 
 
 def truncate_by_token(prompt: str, tokenizer: Encoding, max_token_size: int):
-    tokens = tokenizer.encode(prompt)
+    tokens = tokenizer.encode(prompt, allowed_special='all')
     return tokenizer.decode(tokens[:max_token_size])
---
+++ tests/autorag/nodes/generator/test_openai.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from autorag.nodes.generator import openai_llm
+from tests.autorag.nodes.generator.test_generator_base import prompts, check_generated_texts, check_generated_tokens, \
+    check_generated_log_probs
+
+
+def test_openai_llm():
+    openai_original = openai_llm.__wrapped__
+    model = "gpt-3.5-turbo"
+    answers, tokens, log_probs = openai_original(prompts, model, batch=1)
+    check_generated_texts(answers)
+    check_generated_tokens(tokens)
+    check_generated_log_probs(log_probs)
+
+
+def test_openai_llm_node():
+    previous_result = pd.DataFrame(
+        {
+            'prompts': prompts,
+            'qid': ['id-1', 'id-2', 'id-3']
+        })
+    result_df = openai_llm(project_dir='.', previous_result=previous_result, llm='gpt-3.5-turbo-0125')
+    check_generated_texts(result_df['generated_texts'].tolist())
+    check_generated_tokens(result_df['generated_tokens'].tolist())
+    check_generated_log_probs(result_df['generated_log_probs'].tolist())
+
+
+def test_openai_llm_truncate():
+    openai_original = openai_llm.__wrapped__
+    prompt = [f'havertz on the block and I am {i}th player on the Arsenal.' for i in range(50_000)]
+    prompt = ' '.join(prompt)
+    answers, tokens, log_probs = openai_original([prompt] * 3)
+    check_generated_texts(answers)
+    check_generated_tokens(tokens)
+    check_generated_log_probs(log_probs)
