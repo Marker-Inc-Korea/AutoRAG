391afd6a947b7e487c4af3e340a52b3f35b4323a
jeffrey <vkefhdl1@gmail.com>
2024-05-06 11:15:31 +0900

    mock cohere rerank at test_cohere_reranker.py

--- tests/autorag/nodes/passagereranker/test_cohere_reranker.py
+++ tests/autorag/nodes/passagereranker/test_cohere_reranker.py
@@ -1,9 +1,38 @@
-from autorag.nodes.passagereranker import cohere_reranker
+from unittest.mock import patch
 
+import cohere.base_client
+from cohere import RerankResponse, RerankResponseResultsItem
+
+from autorag.nodes.passagereranker import cohere_reranker
 from tests.autorag.nodes.passagereranker.test_passage_reranker_base import queries_example, contents_example, \
     scores_example, ids_example, base_reranker_test, project_dir, previous_result, base_reranker_node_test
 
 
+async def mock_cohere_reranker(self, model, query, documents, top_n, **kwargs) -> RerankResponse:
+    if query == queries_example[0]:
+        return RerankResponse(
+            results=[
+                        RerankResponseResultsItem(index=1, relevance_score=0.8),
+                        RerankResponseResultsItem(index=2, relevance_score=0.2),
+                        RerankResponseResultsItem(index=0, relevance_score=0.1),
+                    ][:top_n]
+        )
+    elif query == queries_example[1]:
+        return RerankResponse(
+            results=[
+                        RerankResponseResultsItem(index=2, relevance_score=0.8),
+                        RerankResponseResultsItem(index=0, relevance_score=0.2),
+                        RerankResponseResultsItem(index=1, relevance_score=0.1),
+                    ][:top_n]
+        )
+    response_items = [
+        RerankResponseResultsItem(index=i, relevance_score=0.1 * i)
+        for i in range(len(documents) - 1, -1, -1)
+    ]
+    return RerankResponse(results=response_items[:top_n])
+
+
+@patch.object(cohere.base_client.AsyncBaseCohere, "rerank", mock_cohere_reranker)
 def test_cohere_reranker():
     top_k = 3
     original_cohere_reranker = cohere_reranker.__wrapped__
@@ -12,6 +41,7 @@ def test_cohere_reranker():
     base_reranker_test(contents_result, id_result, score_result, top_k)
 
 
+@patch.object(cohere.base_client.AsyncBaseCohere, "rerank", mock_cohere_reranker)
 def test_cohere_reranker_batch_one():
     top_k = 3
     batch = 1
@@ -21,6 +51,7 @@ def test_cohere_reranker_batch_one():
     base_reranker_test(contents_result, id_result, score_result, top_k)
 
 
+@patch.object(cohere.base_client.AsyncBaseCohere, "rerank", mock_cohere_reranker)
 def test_cohere_node():
     top_k = 1
     result_df = cohere_reranker(project_dir=project_dir, previous_result=previous_result, top_k=top_k)
