db7d386ff114981a178e00de89c58473a4295f10
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-05-08 17:53:46 +0900

    Mock cohere and JinaAI test codes (#420)
    * mock cohere rerank at test_cohere_reranker.py
    
    * mock jina ai reranker with aioresponses
    
    * add mock api key at conftest and delete env variable in the test.yml file
    
    * install tests/requirements.txt for testing github actions
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,5 @@ import nest_asyncio
 def pytest_sessionstart(session):
     nest_asyncio.apply()
     os.environ["BM25"] = "bm25"
+    os.environ["JINAAI_API_KEY"] = "mock_jinaai_api_key"
+    os.environ["COHERE_API_KEY"] = "mock_cohere_api_key"
