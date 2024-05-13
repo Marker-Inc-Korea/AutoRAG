95442617283b99613b8a949b461c249148a4a41f
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-05-13 22:35:41 +0900

    Add Ragas context precision metric (#431)
    * made basic ragas context precision retrieval metric
    
    * using ragas context precision at evaluate_retrieval method
    
    * adapt no_gt_metrics at run.py
    
    * add docs about ragas context precision
    
    * build docs
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>

--- autorag/evaluate/retrieval.py
+++ autorag/evaluate/retrieval.py
@@ -1,14 +1,16 @@
 import functools
 import warnings
-from typing import List, Callable, Any, Tuple
+from typing import List, Callable, Any, Tuple, Optional
 
 import pandas as pd
 
 from autorag.evaluate.metric import (retrieval_recall, retrieval_precision, retrieval_f1, retrieval_ndcg, retrieval_mrr,
                                      retrieval_map)
+from autorag.evaluate.metric.retrieval_no_gt import ragas_context_precision
 
 
-def evaluate_retrieval(retrieval_gt: List[List[List[str]]], metrics: List[str]):
+def evaluate_retrieval(retrieval_gt: List[List[List[str]]], metrics: List[str],
+                       queries: Optional[List[str]] = None, generation_gt: Optional[List[List[str]]] = None):
     def decorator_evaluate_retrieval(
             func: Callable[[Any], Tuple[List[List[str]], List[List[str]], List[List[float]]]]):
         """
@@ -32,14 +34,27 @@ def evaluate_retrieval(retrieval_gt: List[List[List[str]]], metrics: List[str]):
                 retrieval_map.__name__: retrieval_map,
             }
 
+            no_gt_metric_funcs = {
+                ragas_context_precision.__name__: ragas_context_precision,
+            }
+
             metric_scores = {}
             for metric in metrics:
-                if metric not in metric_funcs:
-                    warnings.warn(f"metric {metric} is not in supported metrics: {metric_funcs.keys()}"
-                                  f"{metric} will be ignored.")
-                else:
+                if metric in metric_funcs:
                     metric_func = metric_funcs[metric]
                     metric_scores[metric] = metric_func(retrieval_gt=retrieval_gt, pred_ids=pred_ids)
+                elif metric in no_gt_metric_funcs:
+                    metric_func = no_gt_metric_funcs[metric]
+                    if queries is None:
+                        raise ValueError(f"To using {metric}, you have to provide queries.")
+                    if generation_gt is None:
+                        raise ValueError(f"To using {metric}, you have to provide generation ground truth.")
+                    metric_scores[metric] = metric_func(queries=queries, retrieved_contents=contents,
+                                                        generation_gt=generation_gt)
+                else:
+                    warnings.warn(f"metric {metric} is not in supported metrics: {metric_funcs.keys()}"
+                                  f" and {no_gt_metric_funcs.keys()}"
+                                  f"{metric} will be ignored.")
 
             metric_result_df = pd.DataFrame(metric_scores)
             execution_result_df = pd.DataFrame({
