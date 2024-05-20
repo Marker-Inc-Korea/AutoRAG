6eb38a90c90a95efa995f48f0889f486e65f2fc0
jeffrey <vkefhdl1@gmail.com>
2024-05-20 22:41:32 +0900

    get retrieval metric options as list of dict, and test it on the ragas_context_precision metric

--- autorag/evaluate/retrieval.py
+++ autorag/evaluate/retrieval.py
@@ -1,15 +1,21 @@
 import functools
 import warnings
-from typing import List, Callable, Any, Tuple, Optional
+from typing import List, Callable, Any, Tuple, Optional, Union, Dict
 
 import pandas as pd
 
 from autorag.evaluate.metric import (retrieval_recall, retrieval_precision, retrieval_f1, retrieval_ndcg, retrieval_mrr,
                                      retrieval_map)
 from autorag.evaluate.metric.retrieval_no_gt import ragas_context_precision
+from autorag.evaluate.util import cast_metrics
+
+RETRIEVAL_METRIC_FUNC_DICT = {func.__name__: func for func in
+                              [retrieval_recall, retrieval_precision, retrieval_f1, retrieval_ndcg, retrieval_mrr,
+                               retrieval_map]}
+RETRIEVAL_NO_GT_METRIC_FUNC_DICT = {func.__name__: func for func in [ragas_context_precision]}
 
 
-def evaluate_retrieval(retrieval_gt: List[List[List[str]]], metrics: List[str],
+def evaluate_retrieval(retrieval_gt: List[List[List[str]]], metrics: Union[List[str], List[Dict]],
                        queries: Optional[List[str]] = None, generation_gt: Optional[List[List[str]]] = None):
     def decorator_evaluate_retrieval(
             func: Callable[[Any], Tuple[List[List[str]], List[List[str]], List[List[float]]]]):
@@ -25,36 +31,28 @@ def evaluate_retrieval(retrieval_gt: List[List[List[str]]], metrics: List[str],
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> pd.DataFrame:
             contents, pred_ids, scores = func(*args, **kwargs)
-            metric_funcs = {
-                retrieval_recall.__name__: retrieval_recall,
-                retrieval_precision.__name__: retrieval_precision,
-                retrieval_f1.__name__: retrieval_f1,
-                retrieval_ndcg.__name__: retrieval_ndcg,
-                retrieval_mrr.__name__: retrieval_mrr,
-                retrieval_map.__name__: retrieval_map,
-            }
-
-            no_gt_metric_funcs = {
-                ragas_context_precision.__name__: ragas_context_precision,
-            }
 
             metric_scores = {}
-            for metric in metrics:
-                if metric in metric_funcs:
-                    metric_func = metric_funcs[metric]
-                    metric_scores[metric] = metric_func(retrieval_gt=retrieval_gt, pred_ids=pred_ids)
-                elif metric in no_gt_metric_funcs:
-                    metric_func = no_gt_metric_funcs[metric]
+            metric_names, metric_params = cast_metrics(metrics)
+
+            for metric_name, metric_param in zip(metric_names, metric_params):
+                if metric_name in RETRIEVAL_METRIC_FUNC_DICT:
+                    metric_func = RETRIEVAL_METRIC_FUNC_DICT[metric_name]
+                    metric_scores[metric_name] = metric_func(retrieval_gt=retrieval_gt, pred_ids=pred_ids,
+                                                             **metric_param)
+                elif metric_name in RETRIEVAL_NO_GT_METRIC_FUNC_DICT:
+                    metric_func = RETRIEVAL_NO_GT_METRIC_FUNC_DICT[metric_name]
                     if queries is None:
-                        raise ValueError(f"To using {metric}, you have to provide queries.")
+                        raise ValueError(f"To using {metric_name}, you have to provide queries.")
                     if generation_gt is None:
-                        raise ValueError(f"To using {metric}, you have to provide generation ground truth.")
-                    metric_scores[metric] = metric_func(queries=queries, retrieved_contents=contents,
-                                                        generation_gt=generation_gt)
+                        raise ValueError(f"To using {metric_name}, you have to provide generation ground truth.")
+                    metric_scores[metric_name] = metric_func(queries=queries, retrieved_contents=contents,
+                                                             generation_gt=generation_gt, **metric_param)
                 else:
-                    warnings.warn(f"metric {metric} is not in supported metrics: {metric_funcs.keys()}"
-                                  f" and {no_gt_metric_funcs.keys()}"
-                                  f"{metric} will be ignored.")
+                    warnings.warn(
+                        f"metric {metric_name} is not in supported metrics: {RETRIEVAL_METRIC_FUNC_DICT.keys()}"
+                        f" and {RETRIEVAL_NO_GT_METRIC_FUNC_DICT.keys()}"
+                        f"{metric_name} will be ignored.")
 
             metric_result_df = pd.DataFrame(metric_scores)
             execution_result_df = pd.DataFrame({
--- tests/autorag/evaluate/test_retrieval_evaluate.py
+++ tests/autorag/evaluate/test_retrieval_evaluate.py
@@ -13,30 +13,38 @@ generation_gt_example = [['Jazz', 'Music'],
                          ['Havertz'],
                          ['William', 'Gamst'],
                          ['Kia', 'Tigers', 'V12']]
+contents = [
+    ['a', 'b', 'c', 'd'],
+    ['better', 'bone', 'caleb', 'done'],
+    ['ate', 'better', 'cortex', 'dad'],
+    ['aim', 'bond', 'cane', 'dance'],
+]
+scores = [
+    [0.1, 0.2, 0.3, 0.4],
+    [0.4, 0.3, 0.2, 0.1],
+    [0.5, 0.4, 0.3, 0.2],
+    [0.6, 0.5, 0.4, 0.3],
+]
+ids = [
+    [retrieval_gt[0][0][0], retrieval_gt[0][1][0], 'pred-0', 'pred-1'],
+    ['pred-2', retrieval_gt[1][0][0], retrieval_gt[1][1][0], 'pred-3'],
+    [f'pred-{i}' for i in range(4, 8)],
+    [retrieval_gt[3][0][0], 'pred-8', 'pred-9', 'pred-10'],
+]
 
 
 @evaluate_retrieval(retrieval_gt=retrieval_gt,
                     metrics=['retrieval_recall', 'retrieval_precision', 'retrieval_f1', 'ragas_context_precision'],
                     queries=queries_example, generation_gt=generation_gt_example)
 def pseudo_retrieval() -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
-    contents = [
-        ['a', 'b', 'c', 'd'],
-        ['better', 'bone', 'caleb', 'done'],
-        ['ate', 'better', 'cortex', 'dad'],
-        ['aim', 'bond', 'cane', 'dance'],
-    ]
-    scores = [
-        [0.1, 0.2, 0.3, 0.4],
-        [0.4, 0.3, 0.2, 0.1],
-        [0.5, 0.4, 0.3, 0.2],
-        [0.6, 0.5, 0.4, 0.3],
-    ]
-    ids = [
-        [retrieval_gt[0][0][0], retrieval_gt[0][1][0], 'pred-0', 'pred-1'],
-        ['pred-2', retrieval_gt[1][0][0], retrieval_gt[1][1][0], 'pred-3'],
-        [f'pred-{i}' for i in range(4, 8)],
-        [retrieval_gt[3][0][0], 'pred-8', 'pred-9', 'pred-10'],
-    ]
+    return contents, ids, scores
+
+
+@evaluate_retrieval(retrieval_gt=retrieval_gt,
+                    metrics=[{'metric_name': 'retrieval_recall'}, {'metric_name': 'retrieval_f1'},
+                             {'metric_name': 'ragas_context_precision', 'openai_model_name': 'gpt-3.5-turbo-0125'}],
+                    queries=queries_example, generation_gt=generation_gt_example)
+def pseudo_retrieval_dict_metric() -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
     return contents, ids, scores
 
 
@@ -69,5 +77,25 @@ def test_evaluate_retrieval():
 
     ragas_context_precision = result_df['ragas_context_precision'].tolist()
     ragas_context_precision_solution = [0.3] * len(queries_example)
+    for pred, sol in zip(ragas_context_precision, ragas_context_precision_solution):
+        assert math.isclose(pred, sol, rel_tol=1e-7)
+
+
+async def mock_context_precision_ascore_check_model(self, row, callbacks, is_async) -> float:
+    assert self.llm.langchain_llm.model_name == 'gpt-3.5-turbo-0125'
+    return 0.4
+
+
+@patch.object(ContextPrecision, "_ascore", mock_context_precision_ascore_check_model)
+def test_evaluate_retrieval_dict():
+    result_df = pseudo_retrieval_dict_metric()
+    assert isinstance(result_df, pd.DataFrame)
+    assert len(result_df) == 4
+    assert len(result_df.columns) == 6
+    assert set(result_df.columns) == {'retrieved_contents', 'retrieved_ids', 'retrieve_scores', 'retrieval_recall',
+                                      'retrieval_f1', 'ragas_context_precision'}
+
+    ragas_context_precision = result_df['ragas_context_precision'].tolist()
+    ragas_context_precision_solution = [0.4] * len(queries_example)
     for pred, sol in zip(ragas_context_precision, ragas_context_precision_solution):
         assert math.isclose(pred, sol, rel_tol=1e-7)
