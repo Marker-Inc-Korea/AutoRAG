a2a03ffe20b7517b3efac5bd36eee5b91e440a67
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-05-21 11:22:36 +0900

    Delete ragas context precision & Enable dict input of retrieval metrics (#455)
    * change default openai model name to gpt-3.5-turbo
    
    * get retrieval metric options as list of dict, and test it on the ragas_context_precision metric
    
    * add ragas context precision docs on retrieval.md
    
    * delete ragas context precision
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>
    Co-authored-by: Bwook (Byoungwook) Kim <bwook00@naver.com>

--- tests/autorag/evaluate/test_retrieval_evaluate.py
+++ tests/autorag/evaluate/test_retrieval_evaluate.py
@@ -1,9 +1,7 @@
 import math
 from typing import Tuple, List
-from unittest.mock import patch
 
 import pandas as pd
-from ragas.metrics import ContextPrecision
 
 from autorag.evaluate import evaluate_retrieval
 
@@ -13,45 +11,47 @@ generation_gt_example = [['Jazz', 'Music'],
                          ['Havertz'],
                          ['William', 'Gamst'],
                          ['Kia', 'Tigers', 'V12']]
+contents = [
+    ['a', 'b', 'c', 'd'],
+    ['better', 'bone', 'caleb', 'done'],
+    ['ate', 'better', 'cortex', 'dad'],
+    ['aim', 'bond', 'cane', 'dance'],
+]
+scores = [
+    [0.1, 0.2, 0.3, 0.4],
+    [0.4, 0.3, 0.2, 0.1],
+    [0.5, 0.4, 0.3, 0.2],
+    [0.6, 0.5, 0.4, 0.3],
+]
+ids = [
+    [retrieval_gt[0][0][0], retrieval_gt[0][1][0], 'pred-0', 'pred-1'],
+    ['pred-2', retrieval_gt[1][0][0], retrieval_gt[1][1][0], 'pred-3'],
+    [f'pred-{i}' for i in range(4, 8)],
+    [retrieval_gt[3][0][0], 'pred-8', 'pred-9', 'pred-10'],
+]
 
 
 @evaluate_retrieval(retrieval_gt=retrieval_gt,
-                    metrics=['retrieval_recall', 'retrieval_precision', 'retrieval_f1', 'ragas_context_precision'],
+                    metrics=['retrieval_recall', 'retrieval_precision', 'retrieval_f1'],
                     queries=queries_example, generation_gt=generation_gt_example)
 def pseudo_retrieval() -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
-    contents = [
-        ['a', 'b', 'c', 'd'],
-        ['better', 'bone', 'caleb', 'done'],
-        ['ate', 'better', 'cortex', 'dad'],
-        ['aim', 'bond', 'cane', 'dance'],
-    ]
-    scores = [
-        [0.1, 0.2, 0.3, 0.4],
-        [0.4, 0.3, 0.2, 0.1],
-        [0.5, 0.4, 0.3, 0.2],
-        [0.6, 0.5, 0.4, 0.3],
-    ]
-    ids = [
-        [retrieval_gt[0][0][0], retrieval_gt[0][1][0], 'pred-0', 'pred-1'],
-        ['pred-2', retrieval_gt[1][0][0], retrieval_gt[1][1][0], 'pred-3'],
-        [f'pred-{i}' for i in range(4, 8)],
-        [retrieval_gt[3][0][0], 'pred-8', 'pred-9', 'pred-10'],
-    ]
     return contents, ids, scores
 
 
-async def mock_context_precision_ascore(self, row, callbacks, is_async) -> float:
-    return 0.3
+@evaluate_retrieval(retrieval_gt=retrieval_gt,
+                    metrics=[{'metric_name': 'retrieval_recall'}, {'metric_name': 'retrieval_f1'}],
+                    queries=queries_example, generation_gt=generation_gt_example)
+def pseudo_retrieval_dict_metric() -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
+    return contents, ids, scores
 
 
-@patch.object(ContextPrecision, "_ascore", mock_context_precision_ascore)
 def test_evaluate_retrieval():
     result_df = pseudo_retrieval()
     assert isinstance(result_df, pd.DataFrame)
     assert len(result_df) == 4
-    assert len(result_df.columns) == 7
+    assert len(result_df.columns) == 6
     assert list(result_df.columns) == ['retrieved_contents', 'retrieved_ids', 'retrieve_scores', 'retrieval_recall',
-                                       'retrieval_precision', 'retrieval_f1', 'ragas_context_precision']
+                                       'retrieval_precision', 'retrieval_f1']
     recall = result_df['retrieval_recall'].tolist()
     recall_solution = [1, 1, 0, 0.5]
     for pred, sol in zip(recall, recall_solution):
@@ -67,7 +67,16 @@ def test_evaluate_retrieval():
     for pred, sol in zip(f1, f1_solution):
         assert math.isclose(pred, sol, rel_tol=1e-4)
 
-    ragas_context_precision = result_df['ragas_context_precision'].tolist()
-    ragas_context_precision_solution = [0.3] * len(queries_example)
-    for pred, sol in zip(ragas_context_precision, ragas_context_precision_solution):
-        assert math.isclose(pred, sol, rel_tol=1e-7)
+
+def test_evaluate_retrieval_dict():
+    result_df = pseudo_retrieval_dict_metric()
+    assert isinstance(result_df, pd.DataFrame)
+    assert len(result_df) == 4
+    assert len(result_df.columns) == 5
+    assert set(result_df.columns) == {'retrieved_contents', 'retrieved_ids', 'retrieve_scores', 'retrieval_recall',
+                                      'retrieval_f1'}
+
+    recall = result_df['retrieval_recall'].tolist()
+    recall_solution = [1, 1, 0, 0.5]
+    for pred, sol in zip(recall, recall_solution):
+        assert math.isclose(pred, sol, rel_tol=1e-4)
