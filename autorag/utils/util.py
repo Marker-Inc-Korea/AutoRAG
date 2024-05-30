16fd8185b3e49be32f79e885dd5f47ea2b57d058
jeffrey <vkefhdl1@gmail.com>
2024-05-30 10:52:58 +0900

    reset index at split dataframe

--- autorag/utils/util.py
+++ autorag/utils/util.py
@@ -361,7 +361,9 @@ def filter_dict_keys(dict_, keys: List[str]):
 
 def split_dataframe(df, chunk_size):
     num_chunks = len(df) // chunk_size + 1 if len(df) % chunk_size != 0 else len(df) // chunk_size
-    return list(map(lambda x: df[x * chunk_size:(x + 1) * chunk_size], range(num_chunks)))
+    result = list(map(lambda x: df[x * chunk_size:(x + 1) * chunk_size], range(num_chunks)))
+    result = list(map(lambda x: x.reset_index(drop=True), result))
+    return result
 
 
 def find_trial_dir(project_dir: str) -> List[str]:
