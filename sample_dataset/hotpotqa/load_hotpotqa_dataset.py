4fc6b7b98be6b5678640bc58485c44c1062366a1
Christian Di Maio <39567665+gnekt@users.noreply.github.com>
2024-06-17 14:30:27 +0200

    Add HotpotQA Dataset Adapted for AutoRAG Format (#492)
    * Added hotpotqa to the sample_dataset
    
    * slight mod
    
    * fix
    
    * moved to a compact version of hotpotqa

---
+++ sample_dataset/hotpotqa/load_hotpotqa_dataset.py
@@ -0,0 +1,28 @@
+import os
+import pathlib
+
+import click
+from datasets import load_dataset
+
+
+@click.command()
+@click.option('--save_path', type=str, default=pathlib.PurePath(__file__).parent, help='Path to save sample hotpotqa dataset.')
+def load_hotpotqa_dataset(save_path):
+    # set file path
+    file_path = "gnekt/hotpotqa_small_sample_autorag"
+
+    # load dataset
+    corpus_dataset = load_dataset(file_path, "corpus")['train'].to_pandas()
+    qa_validation_dataset = load_dataset(file_path, "qa")['validation'].to_pandas()
+
+    # save corpus data
+    if os.path.exists(os.path.join(save_path, "corpus.parquet")) is True:
+        raise ValueError("corpus.parquet already exists")
+    if os.path.exists(os.path.join(save_path, "qa.parquet")) is True:
+        raise ValueError("qa.parquet already exists")
+    corpus_dataset.to_parquet(os.path.join(save_path, "corpus.parquet"), index=False)
+    qa_validation_dataset.to_parquet(os.path.join(save_path, "qa_validation.parquet"), index=False)
+
+
+if __name__ == '__main__':
+    load_hotpotqa_dataset()
