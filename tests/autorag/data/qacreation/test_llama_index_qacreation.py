f3966a6925a7ca3b0b3d5cdb97029ad0577a7e45
jeffrey <vkefhdl1@gmail.com>
2024-08-14 10:42:37 +0800

    add test code for generate_answers and generate_basic_answer

--- tests/autorag/data/qacreation/test_llama_index_qacreation.py
+++ tests/autorag/data/qacreation/test_llama_index_qacreation.py
@@ -9,7 +9,7 @@ from llama_index.core.base.llms.types import CompletionResponse
 from llama_index.core.llms import MockLLM
 
 from autorag.data.qacreation.llama_index import async_qa_gen_llama_index, generate_qa_llama_index_by_ratio, \
-    generate_qa_llama_index
+    generate_qa_llama_index, generate_basic_answer, generate_answers
 
 root_dir = pathlib.PurePath(os.path.dirname(os.path.realpath(__file__))).parent.parent.parent
 resource_dir = os.path.join(root_dir, "resources")
@@ -28,6 +28,12 @@ sample_prompt = open(os.path.join(prompt_dir, "prompt1.txt")).read()
 def contents():
     df = pd.read_csv(os.path.join(resource_dir, "sample_contents_nqa.csv"))
     yield df['passage'].tolist()[:6]
+
+
+@pytest.fixture
+def questions():
+    df = pd.read_csv(os.path.join(resource_dir, "sample_contents_nqa.csv"))
+    yield df['question'].tolist()[:6]
 
 
 async def acomplete_qa_creation(self, messages, **kwargs):
@@ -67,6 +73,22 @@ def test_qa_gen_llama_index_by_ratio(contents):
     llm = MockLLM()
     result = generate_qa_llama_index_by_ratio(llm, contents, ratio_dict, batch=8)
     check_multi_qa_gen(result)
+
+
+def test_generate_answers(contents, questions):
+    llm = MockLLM()
+    result = generate_answers(llm, contents, questions)
+    assert len(result) == len(contents) == len(questions)
+    assert all(isinstance(res, str) for res in result)
+
+
+@pytest.mark.asyncio()
+async def test_generate_basic_answer():
+    llm = MockLLM()
+    query = "What is Fire and brimstone?"
+    response = await generate_basic_answer(llm, content, query)
+    assert isinstance(response, str)
+    assert bool(response) is True
 
 
 def check_multi_qa_gen(result):
