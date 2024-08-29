225bdd1e6e3ca636cce57191ade9112b1194f656
jeffrey <vkefhdl1@gmail.com>
2024-08-29 17:34:56 +0800

    mock parsed response

--- tests/autorag/data/beta/generation_gt/test_openai_gen_gt.py
+++ tests/autorag/data/beta/generation_gt/test_openai_gen_gt.py
@@ -1,9 +1,18 @@
-from openai import AsyncClient
+import time
+from unittest.mock import patch
 
+import openai.resources.beta.chat
+from openai import AsyncOpenAI
+from openai.types.chat import (
+	ParsedChatCompletion,
+	ParsedChoice,
+	ParsedChatCompletionMessage,
+)
 
 from autorag.data.beta.generation_gt.openai_gen_gt import (
 	make_concise_gen_gt,
 	make_basic_gen_gt,
+	Response,
 )
 from autorag.schema.data import QA
 from tests.autorag.data.beta.generation_gt.base_test_generation_gt import (
@@ -11,9 +20,33 @@ from tests.autorag.data.beta.generation_gt.base_test_generation_gt import (
 	check_generation_gt,
 )
 
-client = AsyncClient()
+client = AsyncOpenAI()
 
 
+async def mock_gen_gt_response(*args, **kwargs) -> ParsedChatCompletion[Response]:
+	return ParsedChatCompletion(
+		id="test_id",
+		choices=[
+			ParsedChoice(
+				finish_reason="stop",
+				index=0,
+				message=ParsedChatCompletionMessage(
+					parsed=Response(answer="mock answer"),
+					role="assistant",
+				),
+			)
+		],
+		created=int(time.time()),
+		model="gpt-4o-mini-2024-07-18",
+		object="chat.completion",
+	)
+
+
+@patch.object(
+	openai.resources.beta.chat.completions.AsyncCompletions,
+	"parse",
+	mock_gen_gt_response,
+)
 def test_make_concise_gen_gt():
 	qa = QA(qa_df)
 	result_qa = qa.batch_apply(
@@ -24,6 +57,11 @@ def test_make_concise_gen_gt():
 	check_generation_gt(result_qa)
 
 
+@patch.object(
+	openai.resources.beta.chat.completions.AsyncCompletions,
+	"parse",
+	mock_gen_gt_response,
+)
 def test_make_basic_gen_gt():
 	qa = QA(qa_df)
 	result_qa = qa.batch_apply(lambda row: make_basic_gen_gt(row, client))
