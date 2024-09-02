dfe7d9f5ed15fdad415207330dc425fb466b04eb
kimbwook <bwook00@naver.com>
2024-09-02 16:35:05 +0900

    delete llama_parse_result check code

--- tests/autorag/data/parse/test_llamaparse.py
+++ tests/autorag/data/parse/test_llamaparse.py
@@ -8,6 +8,7 @@ from tests.autorag.data.parse.test_parse_base import (
 	multiple_pdf_data_list,
 	korean_text_glob,
 	eng_text_glob,
+	check_parse_result,
 )
 
 from llama_index.core.schema import Document
@@ -18,41 +19,23 @@ async def mock_llama_parse_aload_data(*args, **kwargs) -> List[Document]:
 	return [Document(id_="test_id", text="I love AutoRAG", metadata={})]
 
 
-def check_llama_parse_result(texts, file_names, file_type):
-	if file_type == "single_pdf":
-		assert texts == [
-			"I love AutoRAG",
-		]
-		assert file_names == [
-			"korean_texts_two_page.pdf",
-		]
-	elif file_type == "multiple_pdf":
-		assert texts == [
-			"I love AutoRAG",
-			"I love AutoRAG",
-		]
-		expect_names = [
-			"baseball_1.pdf",
-			"baseball_2.pdf",
-		]
-		assert all([file_name in expect_names for file_name in file_names])
-
-
 @patch.object(llama_parse.base.LlamaParse, "aload_data", mock_llama_parse_aload_data)
 def test_llama_parse_single_pdf():
 	llama_parse_original = llamaparse.__wrapped__
 	texts, file_names = llama_parse_original(
 		single_pdf_path_list, url="mock_url", api_key="mock_api_key"
 	)
-	check_llama_parse_result(texts, file_names, "single_pdf")
+	check_parse_result(texts, file_names, "single_pdf")
+	assert texts == ["I love AutoRAG"]
 
 
 @patch.object(llama_parse.base.LlamaParse, "aload_data", mock_llama_parse_aload_data)
 def test_llama_parse_single_pdf_node():
 	result_df = llamaparse(korean_text_glob, url="mock_url", api_key="mock_api_key")
-	check_llama_parse_result(
+	check_parse_result(
 		result_df["texts"].tolist(), result_df["file_name"].tolist(), "single_pdf"
 	)
+	assert result_df["texts"].tolist() == ["I love AutoRAG"]
 
 
 @patch.object(llama_parse.base.LlamaParse, "aload_data", mock_llama_parse_aload_data)
@@ -61,12 +44,14 @@ def test_llama_parse_multiple_pdf():
 	texts, file_names = llama_parse_original(
 		multiple_pdf_data_list, url="mock_url", api_key="mock_api_key"
 	)
-	check_llama_parse_result(texts, file_names, "multiple_pdf")
+	check_parse_result(texts, file_names, "multiple_pdf")
+	assert texts == ["I love AutoRAG", "I love AutoRAG"]
 
 
 @patch.object(llama_parse.base.LlamaParse, "aload_data", mock_llama_parse_aload_data)
 def test_llama_parse_multiple_pdf_node():
 	result_df = llamaparse(eng_text_glob, url="mock_url", api_key="mock_api_key")
-	check_llama_parse_result(
+	check_parse_result(
 		result_df["texts"].tolist(), result_df["file_name"].tolist(), "multiple_pdf"
 	)
+	assert result_df["texts"].tolist() == ["I love AutoRAG", "I love AutoRAG"]
