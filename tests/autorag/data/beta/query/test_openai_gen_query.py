b8aeddc93cedb22cf0374704bc6a980a7262f6cd
jeffrey <vkefhdl1@gmail.com>
2024-09-05 16:02:55 +0800

    add openai query gen test code

---
+++ tests/autorag/data/beta/query/test_openai_gen_query.py
@@ -0,0 +1,49 @@
+import time
+from unittest.mock import patch
+
+import openai.resources.beta.chat
+from openai import AsyncOpenAI
+from openai.types.chat import (
+	ParsedChatCompletion,
+	ParsedChatCompletionMessage,
+	ParsedChoice,
+)
+
+from autorag.data.beta.query.openai_gen_query import Response, factoid_query_gen
+from autorag.schema.data import QA
+from tests.autorag.data.beta.query.base_test_query_gen import qa_df
+
+
+client = AsyncOpenAI()
+
+
+async def mock_gen_gt_response(*args, **kwargs) -> ParsedChatCompletion[Response]:
+	return ParsedChatCompletion(
+		id="test_id",
+		choices=[
+			ParsedChoice(
+				finish_reason="stop",
+				index=0,
+				message=ParsedChatCompletionMessage(
+					parsed=Response(query="mock answer"),
+					role="assistant",
+				),
+			)
+		],
+		created=int(time.time()),
+		model="gpt-4o-mini-2024-07-18",
+		object="chat.completion",
+	)
+
+
+@patch.object(
+	openai.resources.beta.chat.completions.AsyncCompletions,
+	"parse",
+	mock_gen_gt_response,
+)
+def test_make_factoid_query_gen():
+	qa = QA(qa_df)
+	new_qa = qa.batch_apply(lambda row: factoid_query_gen(row, client))
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
