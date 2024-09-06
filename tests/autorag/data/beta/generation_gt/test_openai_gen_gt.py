5d29ab8061c41fbb76adf46a210fbdc95d900a47
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2024-09-06 13:32:56 +0100

    Add more data creation methods (#680)
    * update data schema
    
    * make openai query gen function
    
    * add openai query gen test code
    
    * add korean prompt feature at query gen
    
    * add korean prompt feature at generation gt gen
    
    * change data schema path to inside the data folder
    
    * change linked_raw and linked_corpus to read only value
    
    * delete wrong annotation
    
    * build docs
    
    * add sample functions
    
    * change the kwargs structure at data schema
    
    * build docs
    
    * add llama gen query methods
    
    * add concept completion query gen
    
    * refactor prompt.py to ChatMessage lists
    
    * add openai two hop incremental
    
    * add llama index two hop incremental
    
    * refactor schema location
    
    * add data creation documentation
    
    * allow multiple generation of gen gt
    
    ---------
    
    Co-authored-by: jeffrey <vkefhdl1@gmail.com>

--- tests/autorag/data/beta/generation_gt/test_openai_gen_gt.py
+++ tests/autorag/data/beta/generation_gt/test_openai_gen_gt.py
@@ -14,7 +14,7 @@ from autorag.data.beta.generation_gt.openai_gen_gt import (
 	make_basic_gen_gt,
 	Response,
 )
-from autorag.data.beta.schema.data import QA
+from autorag.data.beta.schema import QA
 from tests.autorag.data.beta.generation_gt.base_test_generation_gt import (
 	qa_df,
 	check_generation_gt,
@@ -75,3 +75,17 @@ def test_make_basic_gen_gt_ko():
 	qa = QA(qa_df)
 	result_qa = qa.batch_apply(make_basic_gen_gt, client=client, lang="ko")
 	check_generation_gt(result_qa)
+
+
+@patch.object(
+	openai.resources.beta.chat.completions.AsyncCompletions,
+	"parse",
+	mock_gen_gt_response,
+)
+def test_make_multiple_gen_gt():
+	qa = QA(qa_df)
+	result_qa = qa.batch_apply(make_basic_gen_gt, client=client, lang="ko").batch_apply(
+		make_concise_gen_gt, client=client
+	)
+	check_generation_gt(result_qa)
+	assert all(len(x) == 2 for x in result_qa.data["generation_gt"].tolist())
---
+++ tests/autorag/data/beta/query/test_llama_gen_query.py
@@ -0,0 +1,60 @@
+from unittest.mock import patch
+
+from llama_index.core.base.llms.types import ChatResponse, ChatMessage, MessageRole
+from llama_index.core.llms import MockLLM
+
+from autorag.data.beta.query.llama_gen_query import (
+	factoid_query_gen,
+	concept_completion_query_gen,
+	two_hop_incremental,
+)
+from autorag.data.beta.schema import QA
+from tests.autorag.data.beta.query.base_test_query_gen import qa_df, multi_hop_qa_df
+
+llm = MockLLM()
+qa = QA(qa_df)
+
+
+def test_make_factoid_query_gen():
+	new_qa = qa.batch_apply(factoid_query_gen, llm=llm)
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
+
+
+def test_make_factoid_query_gen_ko():
+	new_qa = qa.batch_apply(factoid_query_gen, llm=llm, lang="ko")
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
+
+
+def test_concept_completion_query_gen():
+	new_qa = qa.batch_apply(concept_completion_query_gen, llm=llm, lang="ko")
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
+
+
+async def two_hop_incremental_mock_achat(*args, **kwargs) -> ChatResponse:
+	return ChatResponse(
+		message=ChatMessage(
+			role=MessageRole.ASSISTANT,
+			content="""Answer: Tamaulipas
+One-hop question (using Document 1): In which Mexican state is Nuevo Laredo located?
+Two-hop question (using Document 2):  In which Mexican state can one find the Ciudad Deportiva, home to the Tecolotes de Nuevo Laredo?""",
+		)
+	)
+
+
+@patch.object(MockLLM, "achat", two_hop_incremental_mock_achat)
+def test_two_hop_incremental_query_gen():
+	multi_hop_qa = QA(multi_hop_qa_df)
+	new_qa = multi_hop_qa.batch_apply(two_hop_incremental, llm=llm, lang="ko")
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(multi_hop_qa_df)
+	assert (
+		new_qa.data["query"].iloc[0]
+		== "In which Mexican state can one find the Ciudad Deportiva, home to the Tecolotes de Nuevo Laredo?"
+	)
