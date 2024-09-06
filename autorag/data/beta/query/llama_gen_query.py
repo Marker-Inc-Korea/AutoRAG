61035dabbc99bcf68804d9fa11844c9c4a954283
jeffrey <vkefhdl1@gmail.com>
2024-09-06 09:46:57 +0800

    add llama index two hop incremental

--- autorag/data/beta/query/llama_gen_query.py
+++ autorag/data/beta/query/llama_gen_query.py
@@ -42,3 +42,23 @@ async def concept_completion_query_gen(
 	return await llama_index_generate_base(
 		row, llm, QUERY_GEN_PROMPT["concept_completion"][lang]
 	)
+
+
+async def two_hop_incremental(
+	row: Dict,
+	llm: BaseLLM,
+	lang: str = "en",
+) -> Dict:
+	messages = QUERY_GEN_PROMPT["two_hop_incremental"][lang]
+	passages = row["retrieval_gt_contents"]
+	assert (
+		len(passages) >= 2
+	), "You have to sample more than two passages for making two-hop questions."
+	context_str = f"Document 1: {passages[0][0]}\nDocument 2: {passages[1][0]}"
+	user_prompt = f"{context_str}\n\nGenerated two-hop Question from two Documents:\n"
+	messages.append(ChatMessage(role=MessageRole.USER, content=user_prompt))
+
+	chat_response: ChatResponse = await llm.achat(messages=messages)
+	response = chat_response.message.content
+	row["query"] = response.split(":")[-1].strip()
+	return row
--- tests/autorag/data/beta/query/test_llama_gen_query.py
+++ tests/autorag/data/beta/query/test_llama_gen_query.py
@@ -1,11 +1,15 @@
+from unittest.mock import patch
+
+from llama_index.core.base.llms.types import ChatResponse, ChatMessage, MessageRole
 from llama_index.core.llms import MockLLM
 
 from autorag.data.beta.query.llama_gen_query import (
 	factoid_query_gen,
 	concept_completion_query_gen,
+	two_hop_incremental,
 )
 from autorag.data.beta.schema.data import QA
-from tests.autorag.data.beta.query.base_test_query_gen import qa_df
+from tests.autorag.data.beta.query.base_test_query_gen import qa_df, multi_hop_qa_df
 
 llm = MockLLM()
 qa = QA(qa_df)
@@ -30,3 +34,27 @@ def test_concept_completion_query_gen():
 	assert "query" in new_qa.data.columns
 	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
 	assert len(new_qa.data) == len(qa_df)
+
+
+async def two_hop_incremental_mock_achat(*args, **kwargs) -> ChatResponse:
+	return ChatResponse(
+		message=ChatMessage(
+			role=MessageRole.ASSISTANT,
+			content="""Answer: Tamaulipas
+One-hop question (using Document 1): In which Mexican state is Nuevo Laredo located?
+Two-hop question (using Document 2):  In which Mexican state can one find the Ciudad Deportiva, home to the Tecolotes de Nuevo Laredo?""",
+		)
+	)
+
+
+@patch.object(MockLLM, "achat", two_hop_incremental_mock_achat)
+def test_two_hop_incremental_query_gen():
+	multi_hop_qa = QA(multi_hop_qa_df)
+	new_qa = multi_hop_qa.batch_apply(two_hop_incremental, llm=llm, lang="ko")
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(multi_hop_qa_df)
+	assert (
+		new_qa.data["query"].iloc[0]
+		== "In which Mexican state can one find the Ciudad Deportiva, home to the Tecolotes de Nuevo Laredo?"
+	)
