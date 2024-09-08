f007e197168194aaa83c02e7d113d2c82740a399
eastsidegunn <hanpanjjang1@gmail.com>
2024-09-08 15:57:42 +0900

    make check_list method react np.ndarray data format

--- autorag/schema/metricinput.py
+++ autorag/schema/metricinput.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
-from typing import Optional, List, Dict, Callable, Any
+from typing import Optional, List, Dict, Callable, Any, Union
 
+import numpy as np
 import pandas as pd
 
 
@@ -17,16 +18,16 @@ class MetricInput:
     generation_gt: Optional[List[str]] = None
     generated_log_probs: Optional[List[float]] = None
 
-    @classmethod
-    def is_fields_notnone(self, fields_to_check: List[str]) -> bool:
+    def is_fields_notnone(cls, fields_to_check: List[str]) -> bool:
         type_checks: Dict[type, Callable[[Any], bool]] = {
             str: lambda x: len(x.strip()) > 0,
-            list: self._check_list,
+            list: cls._check_list,
             int: lambda _: True,
             float: lambda _: True,
+            np.ndarray: cls._check_list
         }
         for field in fields_to_check:
-            actual_value = getattr(self, field)
+            actual_value = getattr(cls, field)
 
             if actual_value is None:
                 return False
@@ -69,18 +70,31 @@ class MetricInput:
 
         return instances
 
-    def _check_list(self, lst: List[Any]) -> bool:
-        if len(lst) == 0:
+    def _check_list(self, lst_or_arr: Union[List[Any], np.ndarray]) -> bool:
+        if isinstance(lst_or_arr, np.ndarray):
+            lst_or_arr = lst_or_arr.flatten().tolist()
+
+        if len(lst_or_arr) == 0:
             return False
 
-        for item in lst:
-            if isinstance(item, str):
-                if len(item.strip()) == 0:
+        type_checks: Dict[type, Callable[[Any], bool]] = {
+            str: lambda x: len(x.strip()) > 0,
+            list: self._check_list,
+            np.ndarray: self._check_list,
+            int: lambda _: True,
+            float: lambda _: True,
+        }
+
+        for item in lst_or_arr:
+            if item is None:
+                return False
+
+            item_type = type(item)
+
+            if item_type in type_checks:
+                if not type_checks[item_type](item):
                     return False
-            elif isinstance(item, list):
-                if not self._check_list(item):
-                    return False
-            elif item is None:
+            else:
                 return False
 
         return True
