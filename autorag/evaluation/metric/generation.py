55e0420f2a08a171ca4096bcae832c880aa5b8a7
eastsidegunn <hanpanjjang1@gmail.com>
2024-09-26 23:27:02 +0900

    add deepeval_toxicity

--- autorag/evaluation/metric/generation.py
+++ autorag/evaluation/metric/generation.py
@@ -21,6 +21,7 @@ from deepeval.metrics import (
 	FaithfulnessMetric,
 	HallucinationMetric,
 	SummarizationMetric,
+	ToxicityMetric,
 )
 from deepeval.evaluate import evaluate as deepeval_evaluate
 
@@ -579,6 +580,37 @@ def deepeval_summarization(
 	)
 	deepeval_test_results = deepeval_evaluate(
 		test_cases=deepeval_test_cases, metrics=[deepeval_summarization_metric]
+	)
+	results = [
+		convert_deepeval_test_result_to_score(test_result)
+		for test_result in deepeval_test_results
+	]
+
+	return results
+
+
+@autorag_metric_loop(fields_to_check=["query", "generated_texts"])
+def deepeval_toxicity(
+	metric_inputs: List[MetricInput],
+	threshold: float = 0.5,
+	model: Optional[Union[str, DeepEvalBaseLLM]] = None,
+	include_reason: bool = True,
+	async_mode: bool = True,
+	strict_mode: bool = False,
+) -> List[float]:
+	deepeval_test_cases = [
+		metric_input.to_deepeval_testcase() for metric_input in metric_inputs
+	]
+	deepeval_toxicity_metric = ToxicityMetric(
+		threshold=threshold,
+		model=model,
+		include_reason=include_reason,
+		async_mode=async_mode,
+		strict_mode=strict_mode,
+		verbose_mode=False,
+	)
+	deepeval_test_results = deepeval_evaluate(
+		test_cases=deepeval_test_cases, metrics=[deepeval_toxicity_metric]
 	)
 	results = [
 		convert_deepeval_test_result_to_score(test_result)
