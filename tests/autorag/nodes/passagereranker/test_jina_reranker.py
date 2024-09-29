71316f491bcd2348fc64bcb04ffa5671c12d5d3e
김동규 <jeffrey@edai.club>
2024-09-29 19:41:07 +0900

    hotfix jina reranker error about event loop - You have to pass the event loop to the aiohttp.ClientSession()

--- tests/autorag/nodes/passagereranker/test_jina_reranker.py
+++ tests/autorag/nodes/passagereranker/test_jina_reranker.py
@@ -73,7 +73,6 @@ def jina_reranker_instance():
 	return JinaReranker(project_dir, "mock_api_key")
 
 
-@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
 @patch.object(
 	autorag.nodes.passagereranker.jina, "jina_reranker_pure", mock_jina_reranker_pure
 )
@@ -85,7 +84,6 @@ def test_jina_reranker(jina_reranker_instance):
 	base_reranker_test(contents_result, id_result, score_result, top_k)
 
 
-@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
 @patch.object(
 	autorag.nodes.passagereranker.jina, "jina_reranker_pure", mock_jina_reranker_pure
 )
@@ -102,7 +100,6 @@ def test_jina_reranker_batch_one(jina_reranker_instance):
 	base_reranker_test(contents_result, id_result, score_result, top_k)
 
 
-@pytest.mark.skipif(is_github_action(), reason="Skipping this test on GitHub Actions")
 @patch.object(
 	autorag.nodes.passagereranker.jina, "jina_reranker_pure", mock_jina_reranker_pure
 )
