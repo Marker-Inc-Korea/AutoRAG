0f24f84309f8722bfbafa70eb0357c3c51af8216
jeffrey <vkefhdl1@gmail.com>
2024-10-13 01:18:48 +0900

    change the threshold parameter to threshold_datetime at recency filter

--- autorag/nodes/passagefilter/base.py
+++ autorag/nodes/passagefilter/base.py
@@ -1,14 +1,12 @@
 import abc
-import functools
 import logging
-import os
 from pathlib import Path
-from typing import Union, Tuple, List
+from typing import Union
 
 import pandas as pd
 
 from autorag.schema.base import BaseModule
-from autorag.utils import result_to_dataframe, validate_qa_dataset, fetch_contents
+from autorag.utils import validate_qa_dataset
 
 logger = logging.getLogger("AutoRAG")
 
@@ -50,42 +48,3 @@ class BasePassageFilter(BaseModule, metaclass=abc.ABCMeta):
 		), "previous_result must have retrieved_ids column."
 		ids = previous_result["retrieved_ids"].tolist()
 		return queries, contents, scores, ids
-
-
-# same with passage filter from now
-def passage_filter_node(func):
-	@functools.wraps(func)
-	@result_to_dataframe(["retrieved_contents", "retrieved_ids", "retrieve_scores"])
-	def wrapper(
-		project_dir: Union[str, Path], previous_result: pd.DataFrame, *args, **kwargs
-	) -> Tuple[List[List[str]], List[List[str]], List[List[float]]]:
-		if func.__name__ == "recency_filter":
-			corpus_df = pd.read_parquet(
-				os.path.join(project_dir, "data", "corpus.parquet"), engine="pyarrow"
-			)
-			metadatas = fetch_contents(corpus_df, ids, column_name="metadata")
-			times = [
-				[time["last_modified_datetime"] for time in time_list]
-				for time_list in metadatas
-			]
-			filtered_contents, filtered_ids, filtered_scores = func(
-				contents_list=contents,
-				scores_list=scores,
-				ids_list=ids,
-				time_list=times,
-				*args,
-				**kwargs,
-			)
-		else:
-			filtered_contents, filtered_ids, filtered_scores = func(
-				queries=queries,
-				contents_list=contents,
-				scores_list=scores,
-				ids_list=ids,
-				*args,
-				**kwargs,
-			)
-
-		return filtered_contents, filtered_ids, filtered_scores
-
-	return wrapper
