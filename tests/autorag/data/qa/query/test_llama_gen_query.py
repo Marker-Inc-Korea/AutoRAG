9c28822119b1b168f8903f9177c55b28b5c8d850
rjwharry <rjwharry2003@gmail.com>
2024-10-27 16:57:56 +0900

    Add function generating query and gen_gt with custom prompt

--- tests/autorag/data/qa/query/test_llama_gen_query.py
+++ tests/autorag/data/qa/query/test_llama_gen_query.py
@@ -7,6 +7,7 @@ from autorag.data.qa.query.llama_gen_query import (
 	factoid_query_gen,
 	concept_completion_query_gen,
 	two_hop_incremental,
+	custom_query_gen,
 )
 from autorag.data.qa.schema import QA
 from tests.autorag.data.qa.query.base_test_query_gen import qa_df, multi_hop_qa_df
@@ -28,11 +29,13 @@ def test_make_factoid_query_gen_ko():
 	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
 	assert len(new_qa.data) == len(qa_df)
 
+
 def test_make_factoid_query_gen_ja():
-    new_qa = qa.batch_apply(factoid_query_gen, llm=llm, lang="ja")
-    assert "query" in new_qa.data.columns
-    assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
-    assert len(new_qa.data) == len(qa_df)
+	new_qa = qa.batch_apply(factoid_query_gen, llm=llm, lang="ja")
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
+
 
 def test_concept_completion_query_gen():
 	new_qa = qa.batch_apply(concept_completion_query_gen, llm=llm, lang="ko")
@@ -40,12 +43,33 @@ def test_concept_completion_query_gen():
 	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
 	assert len(new_qa.data) == len(qa_df)
 
-def test_concept_completion_query_gen_ja():
-    new_qa = qa.batch_apply(concept_completion_query_gen, llm=llm, lang="ja")
-    assert "query" in new_qa.data.columns
-    assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
-    assert len(new_qa.data) == len(qa_df)
 
+def test_concept_completion_query_gen_ja():
+	new_qa = qa.batch_apply(concept_completion_query_gen, llm=llm, lang="ja")
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
+
+
+def test_custom_query_gen():
+	messages = [
+		ChatMessage(
+			role=MessageRole.SYSTEM,
+			content="""As an expert AI assistant, create one question that can be directly answered from the provided **Text**.
+Ensure question:
+
+- Is clearly based on information within the **Text** and avoids assumptions or external context.
+- Excludes any introductory phrases, such as "In the given text...", "Based on the context...", "Here are some questions baesd on context...".
+- should include question sentence only.
+- Is concise, relevant, and encourages specific, direct answers.
+- has to be same as system language.
+""",
+		)
+	]
+	new_qa = qa.batch_apply(custom_query_gen, llm=llm, messages=messages)
+	assert "query" in new_qa.data.columns
+	assert all(isinstance(query, str) for query in new_qa.data["query"].tolist())
+	assert len(new_qa.data) == len(qa_df)
 
 
 async def two_hop_incremental_mock_achat(*args, **kwargs) -> ChatResponse:
