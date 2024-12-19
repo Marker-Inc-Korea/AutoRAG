08e360d883c7e75b23ba87b3f32b9f694a652bdf
Um Changyong <changyong.um@sfa.co.kr>
2024-12-19 17:08:33 +0900

    feat: apply embeddingbase class to vectordb

--- autorag/embedding/base.py
+++ autorag/embedding/base.py
@@ -1,9 +1,15 @@
+import logging
+
 from random import random
 from typing import List
 
 from llama_index.core.embeddings.mock_embed_model import MockEmbedding
 from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.openai import OpenAIEmbeddingModelType
+from langchain_openai.embeddings import OpenAIEmbeddings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+logger = logging.getLogger("AutoRAG")
 
 class LazyInit:
 	def __init__(self, factory, *args, **kwargs):
@@ -29,21 +35,74 @@ class MockEmbeddingRandom(MockEmbedding):
 		return [random() for _ in range(self.embed_dim)]
 
 class EmbeddingModel:
+
+    @staticmethod
+    def load(name: str = "", **kwargs):
+        
+        embedding_models = {
+            # llama index
+            "openai": LazyInit(
+                OpenAIEmbedding
+            ),  # default model is OpenAIEmbeddingModelType.TEXT_EMBED_ADA_002
+            "openai_embed_3_large": LazyInit(
+                OpenAIEmbedding, model_name=OpenAIEmbeddingModelType.TEXT_EMBED_3_LARGE
+            ),
+            "openai_embed_3_small": LazyInit(
+                OpenAIEmbedding, model_name=OpenAIEmbeddingModelType.TEXT_EMBED_3_SMALL
+            ),
+            "mock": LazyInit(MockEmbeddingRandom, embed_dim=768),
+            # langchain
+            "openai_langchain": LazyInit(OpenAIEmbeddings),
+            # local model
+            "huggingface_baai_bge_small": LazyInit(
+                HuggingFaceEmbedding, model_name="BAAI/bge-small-en-v1.5"
+            ),
+            "huggingface_cointegrated_rubert_tiny2": LazyInit(
+                HuggingFaceEmbedding, model_name="cointegrated/rubert-tiny2"
+            ),
+            "huggingface_all_mpnet_base_v2": LazyInit(
+                HuggingFaceEmbedding,
+                model_name="sentence-transformers/all-mpnet-base-v2",
+                max_length=512,
+            ),
+            "huggingface_bge_m3": LazyInit(
+                HuggingFaceEmbedding, model_name="BAAI/bge-m3"
+            ),
+            # "huggingface_multilingual_e5_large": LazyInit(
+            #     HuggingFaceEmbedding, model_name="intfloat/multilingual-e5-large-instruct"
+            # ),
+        }
+        
+        return embedding_models[name]()
     
     @staticmethod
-    def load_from_dict(**kwargs):
-        if kwargs.get("type", None) == None:
-            raise ValueError("type is not provided")
-        if kwargs.get("model_name", None) == None:
-            raise ValueError("model_name is not provided")
+    def load_from_dict(option: List[dict]):
         
-        _type = kwargs.get("type")
+        def _check_one_item(target: List):
+            if len(target) > 1:
+                raise ValueError("only one embedding model is supported")
+            if len(target) == 0:
+                raise ValueError("embedding model is empty")
+            
+        def _check_keys(target: dict):
+            if target.get("type", None) not in ["openai", "huggingface", "mock"]:
+                raise ValueError("type is not provided")
+            if target.get("model_name", None) is None:
+                raise ValueError("model_name is not provided")
+       
+        _check_one_item(option)
+        _check_keys(option[0])
+
+        _option = option[0]
+        _type = _option.pop("type")
+        # _model_name = _option.pop("model_name")
+        
         _embedding = None
         
         if _type == "openai":
-            _embedding = LazyInit(OpenAIEmbedding, kwargs=kwargs)
+            _embedding = LazyInit(OpenAIEmbedding, **_option)
         elif _type == "huggingface":
-            _embedding = LazyInit(HuggingFaceEmbedding, kwargs=kwargs)
+            _embedding = LazyInit(HuggingFaceEmbedding, **_option)
         elif _type == "mock":
             _embedding = LazyInit(MockEmbeddingRandom, embed_dim=768)
 
@@ -51,3 +110,4 @@ class EmbeddingModel:
             raise ValueError("type is not supported")            
         
         return _embedding
+    
